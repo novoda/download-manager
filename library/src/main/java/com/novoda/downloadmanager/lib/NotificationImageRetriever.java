3b184cda7ab974228469b23ed76c8f95d823aba5
Blundell <blundell.app.dev@gmail.com>
2015-05-07 14:22:12 +0100

    Updates javadoc to explain return type
      - appeases JW with fullstops

--- library/src/main/java/com/novoda/downloadmanager/lib/NotificationImageRetriever.java
+++ library/src/main/java/com/novoda/downloadmanager/lib/NotificationImageRetriever.java
@@ -4,6 +4,10 @@ import android.graphics.Bitmap;
 
 public interface NotificationImageRetriever {
 
+    /**
+     * @param imageUrl The image wanted to be loaded.
+     * @return Bitmap of the imageUrl or null if you fail.
+     */
     Bitmap retrieveImage(String imageUrl);
 
 }
