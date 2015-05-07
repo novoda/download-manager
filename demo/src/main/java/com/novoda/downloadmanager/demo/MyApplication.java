65148a488c41482a57ab032b1a04bb29af5465ad
Blundell <blundell.app.dev@gmail.com>
2015-05-07 14:21:54 +0100

    Uses an okhttp retriever to load images

--- demo/src/main/java/com/novoda/downloadmanager/demo/MyApplication.java
+++ demo/src/main/java/com/novoda/downloadmanager/demo/MyApplication.java
@@ -6,17 +6,48 @@ import android.graphics.BitmapFactory;
 
 import com.novoda.downloadmanager.lib.NotificationImageRetriever;
 import com.novoda.downloadmanager.lib.NotificationImageRetrieverFactory;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MyApplication extends Application implements NotificationImageRetrieverFactory {
 
+    private final OkHttpNotificationImageRetriever imageRetriever = new OkHttpNotificationImageRetriever();
+
     @Override
     public NotificationImageRetriever createNotificationImageRetriever() {
-        return new NotificationImageRetriever() {
-            @Override
-            public Bitmap retrieveImage(String imageUrl) {
-                return BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        return imageRetriever;
+    }
+
+    private static class OkHttpNotificationImageRetriever implements NotificationImageRetriever {
+
+        private final OkHttpClient client;
+
+        public OkHttpNotificationImageRetriever() {
+            client = new OkHttpClient();
+        }
+
+        @Override
+        public Bitmap retrieveImage(String imageUrl) {
+            Request request = new Request.Builder()
+                    .get()
+                    .url(imageUrl)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                InputStream inputStream = response.body().byteStream();
+                try {
+                    return BitmapFactory.decodeStream(inputStream);
+                } finally {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                return null;
             }
-        };
+        }
     }
 
 }
