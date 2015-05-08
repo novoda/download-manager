5807a99b16a9b1829054b75c39019d5a24ba44fa
Blundell <blundell.app.dev@gmail.com>
2015-05-08 14:58:03 +0100

    Initial impl of showing all downloads

--- demo/src/main/java/com/novoda/downloadmanager/demo/MainActivity.java
+++ demo/src/main/java/com/novoda/downloadmanager/demo/MainActivity.java
@@ -1,14 +1,23 @@
 package com.novoda.downloadmanager.demo;
 
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.novoda.downloadmanager.lib.DownloadManager;
+import com.novoda.downloadmanager.lib.Query;
 import com.novoda.downloadmanager.lib.Request;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -24,6 +33,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         downloadManager = new DownloadManager(getContentResolver());
 
+        setupDownloadingExample();
+
+        class Foo {
+            String title;
+            String fileName;
+
+            public Foo(String title, String fileName) {
+                this.title = title;
+                this.fileName = fileName;
+            }
+        }
+
+        final List<Foo> foos = new ArrayList<>();
+        Cursor cursor = downloadManager.query(new Query());
+        while (cursor.moveToNext()) {
+            String title = cursor.getString(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_TITLE));
+            String fileName = cursor.getString(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_LOCAL_FILENAME));
+            foos.add(new Foo(title, fileName));
+        }
+
+        ListView listView = (ListView) findViewById(R.id.main_downloads_list);
+        listView.setAdapter(
+                new BaseAdapter() {
+                    @Override
+                    public int getCount() {
+                        return foos.size();
+                    }
+
+                    @Override
+                    public Foo getItem(int position) {
+                        return foos.get(position);
+                    }
+
+                    @Override
+                    public long getItemId(int position) {
+                        return position;
+                    }
+
+                    @Override
+                    public View getView(int position, View convertView, ViewGroup parent) {
+                        View view = View.inflate(parent.getContext(), R.layout.list_item_download, null);
+
+                        Foo foo = getItem(position);
+                        TextView titleTextView = (TextView) view.findViewById(R.id.download_title_text);
+                        TextView locationTextView = (TextView) view.findViewById(R.id.download_location_text);
+
+                        titleTextView.setText(foo.title);
+                        locationTextView.setText(foo.fileName);
+
+                        return view;
+                    }
+                });
+    }
+
+    private void setupDownloadingExample() {
         Uri uri = Uri.parse(BIG_FILE);
         final Request request = new Request(uri);
         request.setDestinationInInternalFilesDir(this, Environment.DIRECTORY_MOVIES, "podcast.mp3");
@@ -33,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         request.setDescription("Nothing to do with beards.");
         request.setMimeType("audio/mp3");
 
-        findViewById(R.id.button).setOnClickListener(
+        findViewById(R.id.main_download_button).setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
