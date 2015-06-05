30fabbb30733a41bc526110e918ccb11a5b582db
Conor O'Donnell <mrconor@gmail.com>
2015-06-05 14:33:16 +0100

    Cache requested bitmap for notifications

--- library/src/main/java/com/novoda/downloadmanager/lib/OkHttpNotificationImageRetriever.java
+++ library/src/main/java/com/novoda/downloadmanager/lib/OkHttpNotificationImageRetriever.java
@@ -14,21 +14,34 @@ class OkHttpNotificationImageRetriever implements NotificationImageRetriever {
 
     private final OkHttpClient client;
 
+    private String imageUrl;
+    private Bitmap bitmap;
+
     public OkHttpNotificationImageRetriever() {
         client = new OkHttpClient();
     }
 
     @Override
     public Bitmap retrieveImage(String imageUrl) {
+        if (imageUrl.equals(this.imageUrl)) {
+            return bitmap;
+        }
+
+        this.imageUrl = imageUrl;
+        if (bitmap != null && !bitmap.isRecycled()) {
+            bitmap.recycle();
+            bitmap = null;
+        }
         Request request = new Request.Builder()
                 .get()
-                .url(imageUrl)
+                .url(this.imageUrl)
                 .build();
         try {
             Response response = client.newCall(request).execute();
             InputStream inputStream = response.body().byteStream();
             try {
-                return BitmapFactory.decodeStream(inputStream);
+                bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
             } finally {
                 inputStream.close();
             }
