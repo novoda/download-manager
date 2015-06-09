25c7a372eb125d93761ac9feddf1e3730422340a
Xavi Rigau <xrigau@gmail.com>
2015-06-09 16:42:51 +0200

    Boyscout - removes unnecessary `final` modifier

--- library/src/main/java/com/novoda/downloadmanager/lib/DownloadProvider.java
+++ library/src/main/java/com/novoda/downloadmanager/lib/DownloadProvider.java
@@ -1221,29 +1221,28 @@ public final class DownloadProvider extends ContentProvider {
         }
     }
 
-    private static final void copyInteger(String key, ContentValues from, ContentValues to) {
+    private static void copyInteger(String key, ContentValues from, ContentValues to) {
         Integer i = from.getAsInteger(key);
         if (i != null) {
             to.put(key, i);
         }
     }
 
-    private static final void copyBoolean(String key, ContentValues from, ContentValues to) {
+    private static void copyBoolean(String key, ContentValues from, ContentValues to) {
         Boolean b = from.getAsBoolean(key);
         if (b != null) {
             to.put(key, b);
         }
     }
 
-    private static final void copyString(String key, ContentValues from, ContentValues to) {
+    private static void copyString(String key, ContentValues from, ContentValues to) {
         String s = from.getAsString(key);
         if (s != null) {
             to.put(key, s);
         }
     }
 
-    private static final void copyStringWithDefault(String key, ContentValues from,
-                                                    ContentValues to, String defaultValue) {
+    private static void copyStringWithDefault(String key, ContentValues from, ContentValues to, String defaultValue) {
         copyString(key, from, to);
         if (!to.containsKey(key)) {
             to.put(key, defaultValue);
