1753459533b2782bbd2353cb58bccca00157267c
Ferran Garriga <zegnus@gmail.com>
2015-06-18 16:29:20 +0100

    Add javadoc to the Fuzz class

--- library/src/main/java/com/novoda/downloadmanager/lib/Fuzz.java
+++ library/src/main/java/com/novoda/downloadmanager/lib/Fuzz.java
@@ -1,5 +1,9 @@
 package com.novoda.downloadmanager.lib;
 
+/**
+ * This class generates a random value to be used as a randomized restart time
+ * for the server queries
+ */
 class Fuzz {
     int getFuzz() {
         return Helpers.sRandom.nextInt(1001);
