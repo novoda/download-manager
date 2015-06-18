524f0a0317b29929e96b7253f9518d72e0af6754
Blundell <blundell.app.dev@gmail.com>
2015-06-18 18:50:04 +0100

    explain why you would override this method

--- demo_serial/src/main/java/com/novoda/downloadmanager/demo/serial/DemoSerialApplication.java
+++ demo_serial/src/main/java/com/novoda/downloadmanager/demo/serial/DemoSerialApplication.java
@@ -7,6 +7,8 @@ import com.novoda.downloadmanager.lib.DownloadClientReadyChecker;
 public class DemoSerialApplication extends Application implements DownloadClientReadyChecker {
     @Override
     public boolean isReadyToDownload() {
+        // Here you would add any reasons you may not want to download
+        // For instance if you have some type of geo-location lock on your download capability
         return true;
     }
 }
