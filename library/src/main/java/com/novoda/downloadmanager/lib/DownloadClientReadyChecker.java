4fe87aab7dcee09960222da08ccf3a7631aafad8
Ferran Garriga <zegnus@gmail.com>
2015-06-19 13:21:56 +0100

    Adds javadoc for documenting that the method isAllowedToDownload will be called in a background thread

--- library/src/main/java/com/novoda/downloadmanager/lib/DownloadClientReadyChecker.java
+++ library/src/main/java/com/novoda/downloadmanager/lib/DownloadClientReadyChecker.java
@@ -3,6 +3,9 @@ package com.novoda.downloadmanager.lib;
 public interface DownloadClientReadyChecker {
     Ready READY = new Ready();
 
+    /**
+     * Warning: This method is executed on a background thread
+     */
     boolean isAllowedToDownload();
 
     /**
