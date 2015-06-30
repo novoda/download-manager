0207f156d7109fb642015c3c60eb43f9b890ea76
Adam Brown <adam@novoda.com>
2015-06-30 15:14:56 +0100

    removing final so the class can be mocked

--- library/src/main/java/com/novoda/downloadmanager/lib/CollatedDownloadInfo.java
+++ library/src/main/java/com/novoda/downloadmanager/lib/CollatedDownloadInfo.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public final class CollatedDownloadInfo {
+public class CollatedDownloadInfo {
 
     private final long totalSize;
 
