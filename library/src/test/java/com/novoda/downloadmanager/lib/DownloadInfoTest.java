5b82feec3d0e48f15008e3d46fcd872369668149
Adam Brown <adam@novoda.com>
2015-07-03 14:16:40 +0100

    applying api change to tests

--- library/src/test/java/com/novoda/downloadmanager/lib/DownloadInfoTest.java
+++ library/src/test/java/com/novoda/downloadmanager/lib/DownloadInfoTest.java
@@ -3,19 +3,20 @@ package com.novoda.downloadmanager.lib;
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class DownloadInfoTest {
 
     private static final DownloadClientReadyChecker IS_READY = new DownloadClientReadyChecker() {
         @Override
-        public boolean isAllowedToDownload(DownloadBatch downloadBatch) {
+        public boolean isAllowedToDownload(Download download) {
             return true;
         }
     };
 
     private static final DownloadClientReadyChecker IS_NOT_READY = new DownloadClientReadyChecker() {
         @Override
-        public boolean isAllowedToDownload(DownloadBatch downloadBatch) {
+        public boolean isAllowedToDownload(Download download) {
             return false;
         }
     };
@@ -31,7 +32,7 @@ public class DownloadInfoTest {
                 IS_READY,
                 null);
 
-        boolean isReady = downloadInfo.isReadyToDownload(null);
+        boolean isReady = downloadInfo.isReadyToDownload(mock(DownloadBatch.class));
 
         assertThat(isReady).isTrue();
     }
@@ -47,7 +48,7 @@ public class DownloadInfoTest {
                 IS_NOT_READY,
                 null);
 
-        boolean isReady = downloadInfo.isReadyToDownload(null);
+        boolean isReady = downloadInfo.isReadyToDownload(mock(DownloadBatch.class));
 
         assertThat(isReady).isFalse();
     }
