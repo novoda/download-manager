ca5b7ba786507c04e01a31cd6b55cb77f0ed5396
Andrei Catinean <andrei@novoda.com>
2015-07-08 22:36:50 +0100

    Making Criteria package local

--- library/src/main/java/com/novoda/downloadmanager/lib/Criteria.java
+++ library/src/main/java/com/novoda/downloadmanager/lib/Criteria.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Criteria {
+class Criteria {
 
     public static final Criteria ALL = new Criteria(null, null);
 
