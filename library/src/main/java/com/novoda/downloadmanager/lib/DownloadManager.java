d81eda1e1ac54dde6c268d56f09885383233c957
Xavi Rigau <xavi@novoda.com>
2015-10-13 16:52:10 +0200

    Extracts methods for a more readable code

--- library/src/main/java/com/novoda/downloadmanager/lib/DownloadManager.java
+++ library/src/main/java/com/novoda/downloadmanager/lib/DownloadManager.java
@@ -1099,8 +1099,7 @@ public class DownloadManager {
         }
 
         private long getErrorCode(int status) {
-            if (400 <= status && status < DownloadStatus.MIN_ARTIFICIAL_ERROR_STATUS
-                    || 500 <= status && status < 600) {
+            if (isHttpClientError(status) || isHttpServerError(status)) {
                 // HTTP status code
                 return status;
             }
@@ -1134,6 +1133,14 @@ public class DownloadManager {
                 default:
                     return ERROR_UNKNOWN;
             }
+        }
+
+        private boolean isHttpClientError(int status) {
+            return 400 <= status && status < DownloadStatus.MIN_ARTIFICIAL_ERROR_STATUS;
+        }
+
+        private boolean isHttpServerError(int status) {
+            return 500 <= status && status < 600;
         }
 
         private int translateStatus(int status) {
