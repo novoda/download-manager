1fe5fa6815ea276587101176620e34e2aefa3d8b
Xavi Rigau <xavi@novoda.com>
2015-10-15 12:09:20 +0200

    Fixes non compiling code from the demos

--- demo-extended/src/main/java/com/novoda/downloadmanager/demo/extended/DemoNotificationCustomiser.java
+++ demo-extended/src/main/java/com/novoda/downloadmanager/demo/extended/DemoNotificationCustomiser.java
@@ -18,12 +18,12 @@ class DemoNotificationCustomiser implements NotificationCustomiser {
     }
 
     @Override
-    public void modifyQueuedOrDownloadingNotification(NotificationCompat.Builder builder, long batchId) {
+    public void modifyQueuedOrDownloadingNotification(NotificationCompat.Builder builder, com.novoda.downloadmanager.Download download) {
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Action action = new NotificationCompat.Action(R.drawable.ic_notif_info, "View", pendingIntent);
         builder.addAction(action);
-        Intent cancelIntent = DownloadManager.createCancelBatchIntent(batchId, context);
+        Intent cancelIntent = DownloadManager.createCancelBatchIntent(download.getId(), context);
         PendingIntent pendingCancelIntent = PendingIntent.getBroadcast(context, 0, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.addAction(com.novoda.downloadmanager.R.drawable.dl__ic_action_cancel, context.getString(com.novoda.downloadmanager.R.string.dl__cancel), pendingCancelIntent);
     }
--- library/src/main/java/com/novoda/downloadmanager/lib/DefaultNotficationCustomiser.java
+++ library/src/main/java/com/novoda/downloadmanager/lib/DefaultNotficationCustomiser.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
+import com.novoda.downloadmanager.Download;
 import com.novoda.downloadmanager.R;
 
 class DefaultNotficationCustomiser implements NotificationCustomiser {
@@ -16,8 +17,8 @@ class DefaultNotficationCustomiser implements NotificationCustomiser {
     }
 
     @Override
-    public void modifyQueuedOrDownloadingNotification(NotificationCompat.Builder builder, long batchId) {
-        Intent cancelIntent = DownloadManager.createCancelBatchIntent(batchId, context);
+    public void modifyQueuedOrDownloadingNotification(NotificationCompat.Builder builder, Download download) {
+        Intent cancelIntent = DownloadManager.createCancelBatchIntent(download.getId(), context);
         PendingIntent pendingCancelIntent = PendingIntent.getBroadcast(context, 0, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.addAction(R.drawable.dl__ic_action_cancel, context.getString(R.string.dl__cancel), pendingCancelIntent);
     }
