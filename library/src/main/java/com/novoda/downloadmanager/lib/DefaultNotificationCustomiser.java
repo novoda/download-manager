8efe56d865f3cf8405988d0ae043750e18cd0bd6
Xavi Rigau <xavi@novoda.com>
2015-10-15 14:34:05 +0200

    Renames class cause there was a typo

---
+++ library/src/main/java/com/novoda/downloadmanager/lib/DefaultNotificationCustomiser.java
@@ -0,0 +1,44 @@
+package com.novoda.downloadmanager.lib;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import com.novoda.downloadmanager.Download;
+import com.novoda.downloadmanager.R;
+
+class DefaultNotificationCustomiser implements NotificationCustomiser {
+
+    private final Context context;
+
+    public DefaultNotificationCustomiser(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void modifyQueuedNotification(NotificationCompat.Builder builder, Download download) {
+        addCancelButton(builder, download);
+    }
+
+    private void addCancelButton(NotificationCompat.Builder builder, Download download) {
+        Intent cancelIntent = DownloadManager.createCancelBatchIntent(download.getId(), context);
+        PendingIntent pendingCancelIntent = PendingIntent.getBroadcast(context, 0, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.addAction(R.drawable.dl__ic_action_cancel, context.getString(R.string.dl__cancel), pendingCancelIntent);
+    }
+
+    @Override
+    public void modifyDownloadingNotification(NotificationCompat.Builder builder, Download download) {
+        addCancelButton(builder, download);
+    }
+
+    @Override
+    public void modifyCompletedNotification(NotificationCompat.Builder builder, Download download) {
+
+    }
+
+    @Override
+    public void modifyCancelledNotification(NotificationCompat.Builder builder, Download download) {
+
+    }
+}
