89fff8b2730bb9393fcdf5610ce86fc8db8e4697
Xavi Rigau <xavi@novoda.com>
2015-10-15 12:45:34 +0200

    Rename all the things to `BeardBatch`

--- demo-extended/src/main/java/com/novoda/downloadmanager/demo/extended/batches/BeardBatch.java
+++ demo-extended/src/main/java/com/novoda/downloadmanager/demo/extended/batches/BeardBatch.java
@@ -8,7 +8,7 @@ import com.novoda.downloadmanager.lib.DownloadManager;
  * The difference between this and {@link BeardDownload} is that the batch doesn't
  * contain the file path as it represents a batch of downloads.
  */
-public class Batch {
+public class BeardBatch {
     private final int id;
     private final String title;
     private final int status;
@@ -16,7 +16,7 @@ public class Batch {
     private final long currentBytes;
     private final String extraData;
 
-    public Batch(int id, String title, int status, long totalBytes, long currentBytes, String extraData) {
+    public BeardBatch(int id, String title, int status, long totalBytes, long currentBytes, String extraData) {
         this.id = id;
         this.title = title;
         this.status = status;
