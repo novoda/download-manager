524de40ecf17cf600ede47f65053de11870d2981
Ferran Garriga <zegnus@gmail.com>
2015-11-19 16:28:18 +0000

    Remove unnecessary import

--- library/src/main/java/com/novoda/downloadmanager/lib/DownloadService.java
+++ library/src/main/java/com/novoda/downloadmanager/lib/DownloadService.java
@@ -46,7 +46,6 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
