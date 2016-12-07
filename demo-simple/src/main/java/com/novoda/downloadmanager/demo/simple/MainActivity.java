20f7e3dfb5baf4edf7a5ddae8a39dde6fb9d71e9
Adam Brown <adam@novoda.com>
2016-12-07 17:03:38 +0000

    trying to simplify the demo

--- demo-simple/src/main/java/com/novoda/downloadmanager/demo/simple/MainActivity.java
+++ demo-simple/src/main/java/com/novoda/downloadmanager/demo/simple/MainActivity.java
@@ -1,7 +1,6 @@
 package com.novoda.downloadmanager.demo.simple;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,9 +8,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.novoda.downloadmanager.Downloader;
-import com.novoda.downloadmanager.OnDownloadsChangedListener;
 import com.novoda.downloadmanager.OnDownloadsUpdateListener;
-import com.novoda.downloadmanager.WatchType;
 import com.novoda.downloadmanager.demo.R;
 import com.novoda.downloadmanager.domain.Download;
 import com.novoda.downloadmanager.domain.DownloadRequest;
@@ -21,19 +18,21 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String BIG_FILE = "http://ipv4.download.thinkbroadband.com/100MB.zip";
-    private static final String SMALL_FILE = "http://ipv4.download.thinkbroadband.com/5MB.zip";
-    private static final String PENGUINS_IMAGE = "http://i.imgur.com/Y7pMO5Kb.jpg";
+    private static final String BIG_FILE_URL = "http://ipv4.download.thinkbroadband.com/100MB.zip";
+    private static final String SMALL_FILE_URL = "http://ipv4.download.thinkbroadband.com/5MB.zip";
+    private static final String PENGUINS_IMAGE_URL = "http://i.imgur.com/Y7pMO5Kb.jpg";
 
-    private Downloader downloader;
     private View emptyView;
     private DownloadAdapter adapter;
+
+    private DownloaderHelper downloaderHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        downloader = new Downloader.Builder().build(this);
+        Downloader downloader = new Downloader.Builder().build(this);
+        downloaderHelper = new DownloaderHelper(downloader);
 
         emptyView = findViewById(R.id.main_no_downloads_view);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.main_downloads_list);
@@ -41,27 +40,27 @@ public class MainActivity extends AppCompatActivity {
         adapter = new DownloadAdapter(onDownloadClickedListener);
         recyclerView.setAdapter(adapter);
 
-        setupDownloadingExample();
-        setupQueryingExample();
+        findViewById(R.id.main_download_button).setOnClickListener(downloadClickListener);
     }
 
-    private void setupDownloadingExample() {
-        findViewById(R.id.main_download_button).setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(@NonNull View v) {
-                        DownloadRequest.File bigFile = createFileRequest(BIG_FILE, "super_big.file");
-                        DownloadRequest.File bigFile2 = createFileRequest(PENGUINS_IMAGE, "penguins.important");
-                        DownloadRequest.File bigFile3 = createFileRequest(SMALL_FILE, "very.small");
-                        DownloadRequest downloadRequest = new DownloadRequest.Builder()
-                                .with(downloader.createDownloadId())
-                                .withFile(bigFile)
-                                .withFile(bigFile2)
-                                .withFile(bigFile3)
-                                .build();
-                        downloader.submit(downloadRequest);
-                    }
-                });
+    private final View.OnClickListener downloadClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            DownloadRequest downloadRequest = createDownloadRequest();
+            downloaderHelper.submit(downloadRequest);
+        }
+    };
+
+    private DownloadRequest createDownloadRequest() {
+        DownloadRequest.File bigFile = createFileRequest(BIG_FILE_URL, "super_big.file");
+        DownloadRequest.File bigFile2 = createFileRequest(PENGUINS_IMAGE_URL, "penguins.important");
+        DownloadRequest.File bigFile3 = createFileRequest(SMALL_FILE_URL, "very.small");
+        return new DownloadRequest.Builder()
+                .with(downloaderHelper.createDownloadId())
+                .withFile(bigFile)
+                .withFile(bigFile2)
+                .withFile(bigFile3)
+                .build();
     }
 
     private DownloadRequest.File createFileRequest(String uri, String filename) {
@@ -69,9 +68,38 @@ public class MainActivity extends AppCompatActivity {
         return new DownloadRequest.File(uri, file.getAbsolutePath(), filename);
     }
 
-    private void setupQueryingExample() {
-        downloader.addOnDownloadsUpdateListener(onDownloadsUpdate);
-        downloader.requestDownloadsUpdate();
+    private final DownloadAdapter.OnDownloadClickedListener onDownloadClickedListener = new DownloadAdapter.OnDownloadClickedListener() {
+        @Override
+        public void onDownloadClicked(Download download) {
+            switch (download.getStatus()) {
+                case RUNNING:
+                    toastMessage("Pausing download!");
+                    downloaderHelper.pause(download.getId());
+                    break;
+
+                case PAUSED:
+                    toastMessage("Resuming download!");
+                    downloaderHelper.resume(download.getId());
+                    break;
+            }
+        }
+
+        private void toastMessage(String message) {
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        downloaderHelper.startWatching(onDownloadsUpdate);
+        downloaderHelper.requestDownloadsUpdate();
+    }
+
+    @Override
+    protected void onPause() {
+        downloaderHelper.stopWatching(onDownloadsUpdate);
+        super.onPause();
     }
 
     private final OnDownloadsUpdateListener onDownloadsUpdate = new OnDownloadsUpdateListener() {
@@ -82,40 +110,4 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private final DownloadAdapter.OnDownloadClickedListener onDownloadClickedListener = new DownloadAdapter.OnDownloadClickedListener() {
-        @Override
-        public void onDownloadClicked(Download download) {
-            switch (download.getStatus()) {
-                case RUNNING:
-                    Toast.makeText(MainActivity.this, "Pausing download!", Toast.LENGTH_SHORT).show();
-                    downloader.pause(download.getId());
-                    break;
-
-                case PAUSED:
-                    Toast.makeText(MainActivity.this, "Resuming download!", Toast.LENGTH_SHORT).show();
-                    downloader.resume(download.getId());
-                    break;
-            }
-        }
-    };
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        downloader.startListeningForDownloadUpdates(WatchType.PROGRESS, onDownloadsChangedListener);
-    }
-
-    private final OnDownloadsChangedListener onDownloadsChangedListener = new OnDownloadsChangedListener() {
-        @Override
-        public void onDownloadsChanged() {
-            downloader.requestDownloadsUpdate();
-        }
-    };
-
-    @Override
-    protected void onPause() {
-        downloader.removeOnDownloadsUpdateListener(onDownloadsUpdate);
-        downloader.stopListeningForDownloadUpdates(onDownloadsChangedListener);
-        super.onPause();
-    }
 }
