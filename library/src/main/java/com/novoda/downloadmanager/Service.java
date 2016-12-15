02e4cd00859751a61bb84816b62750feb2e2ea7b
Adam Brown <adam@novoda.com>
2016-12-15 19:28:20 +0000

    delegating all the logic of the serivce outside so its testable

--- library/src/main/java/com/novoda/downloadmanager/Service.java
+++ library/src/main/java/com/novoda/downloadmanager/Service.java
@@ -1,5 +1,6 @@
 package com.novoda.downloadmanager;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.ContentObserver;
 import android.os.Handler;
@@ -23,10 +24,7 @@ import static android.text.format.DateUtils.MINUTE_IN_MILLIS;
 
 public class Service extends android.app.Service {
 
-    private HandlerThread updateThread;
-    private Handler updateHandler;
-    private ExecutorService executor;
-    private DownloadUpdater downloadUpdater;
+    private Delegate delegate;
 
     @Nullable
     @Override
@@ -39,82 +37,117 @@ public class Service extends android.app.Service {
         super.onCreate();
         Log.e("!!!", "service created");
         DownloadExecutorFactory factory = new DownloadExecutorFactory();
-        executor = factory.createExecutor();
+        ExecutorService executor = factory.createExecutor();
+
+        HandlerThread updateThread = new HandlerThread("DownloadManager-UpdateThread");
+        updateThread.start();
+        Handler updateHandler = new Handler(updateThread.getLooper());
+
+        DownloadUpdater downloadUpdater = createDownloadUpdater(executor, null);
+
+        delegate = new Delegate(updateThread, updateHandler, executor, downloadUpdater, getContentResolver(), this);
+
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        GlobalClientCheck globalClientCheck = Classes.from(intent.getSerializableExtra("foo"));
-        DownloadCheck downloadCheck = Classes.from(intent.getSerializableExtra("bar"));
-        ClientCheckResult clientCheckResult = globalClientCheck.onGlobalCheck();
-
-        if (clientCheckResult.isAllowed()) {
-            downloadUpdater = createDownloadUpdater(downloadCheck);
-            startMonitoringDownloadChanges();
-
-            updateHandler.post(updateCallback);
-        } else {
-            stopSelf();
-        }
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    private DownloadUpdater createDownloadUpdater(DownloadCheck downloadCheck) {
+    private DownloadUpdater createDownloadUpdater(ExecutorService executor, DownloadCheck downloadCheck) {
         DownloadHandler downloadHandler = DownloadHandlerCreator.create(getContentResolver());
         Pauser pauser = new Pauser(LocalBroadcastManager.getInstance(this));
         return new DownloadUpdater(downloadHandler, executor, pauser, downloadCheck);
     }
 
-    private void startMonitoringDownloadChanges() {
-        updateThread = new HandlerThread("DownloadManager-UpdateThread");
-        updateThread.start();
-        updateHandler = new Handler(updateThread.getLooper());
-        getContentResolver().registerContentObserver(Provider.DOWNLOAD_SERVICE_UPDATE, true, contentObserver);
-    }
-
-    private final ContentObserver contentObserver = new ContentObserver(updateHandler) {
-        @Override
-        public void onChange(boolean selfChange) {
-            update();
-        }
-    };
-
-    private final Runnable updateCallback = new Runnable() {
-        @Override
-        public void run() {
-            Log.e("!!!", "update callback triggered");
-            android.os.Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
-            boolean isActive = update();
-            if (isActive) {
-                scheduleUpdate();
-            } else {
-                shutDown();
-            }
-        }
-    };
-
-    private void scheduleUpdate() {
-        updateHandler.removeCallbacks(updateCallback);
-        updateHandler.postDelayed(updateCallback, 5 * MINUTE_IN_MILLIS);
-    }
-
-    private boolean update() {
-        Log.e("!!!", "update!");
-        return downloadUpdater.update();
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        delegate.onStartCommand(intent, flags, startId);
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
-        shutDown();
+        delegate.shutDown();
         super.onDestroy();
     }
 
-    private void shutDown() {
-        Log.e("!!!", "shutting down service");
-        getContentResolver().unregisterContentObserver(contentObserver);
-        executor.shutdownNow();
-        updateThread.quit();
-        stopSelf();
+    private static class Delegate {
+
+        private final HandlerThread updateThread;
+        private final Handler updateHandler;
+        private final ExecutorService executor;
+        private final DownloadUpdater downloadUpdater;
+        private final ContentResolver contentResolver;
+        private final Service service;
+
+        private ContentObserver contentObserver;
+
+        private Delegate(HandlerThread updateThread,
+                         Handler updateHandler,
+                         ExecutorService executor,
+                         DownloadUpdater downloadUpdater,
+                         ContentResolver contentResolver,
+                         Service service) {
+            this.updateThread = updateThread;
+            this.updateHandler = updateHandler;
+            this.executor = executor;
+            this.downloadUpdater = downloadUpdater;
+            this.contentResolver = contentResolver;
+            this.service = service;
+        }
+
+        public void onStartCommand(Intent intent, int flags, int startId) {
+            GlobalClientCheck globalClientCheck = Classes.from(intent.getSerializableExtra("foo"));
+            DownloadCheck downloadCheck = Classes.from(intent.getSerializableExtra("bar"));
+            ClientCheckResult clientCheckResult = globalClientCheck.onGlobalCheck();
+
+            if (clientCheckResult.isAllowed()) {
+                startMonitoringDownloadChanges();
+
+                updateHandler.post(updateCallback);
+            } else {
+                service.stopSelf();
+            }
+        }
+
+        private void startMonitoringDownloadChanges() {
+            contentObserver = new ContentObserver(updateHandler) {
+                @Override
+                public void onChange(boolean selfChange) {
+                    update();
+                }
+            };
+            contentResolver.registerContentObserver(Provider.DOWNLOAD_SERVICE_UPDATE, true, contentObserver);
+        }
+
+        private final Runnable updateCallback = new Runnable() {
+            @Override
+            public void run() {
+                Log.e("!!!", "update callback triggered");
+                android.os.Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+                boolean isActive = update();
+                if (isActive) {
+                    scheduleUpdate();
+                } else {
+                    shutDown();
+                }
+            }
+        };
+
+        private void scheduleUpdate() {
+            updateHandler.removeCallbacks(updateCallback);
+            updateHandler.postDelayed(updateCallback, 5 * MINUTE_IN_MILLIS);
+        }
+
+        private boolean update() {
+            Log.e("!!!", "update!");
+            return downloadUpdater.update();
+        }
+
+        public void shutDown() {
+            Log.e("!!!", "shutting down service");
+            contentResolver.unregisterContentObserver(contentObserver);
+            executor.shutdownNow();
+            updateThread.quit();
+            service.stopSelf();
+        }
+
     }
 
 }
