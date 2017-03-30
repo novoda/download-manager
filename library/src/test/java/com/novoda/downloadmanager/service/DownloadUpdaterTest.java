a7bf46630b98840f89edd2bb4ee3372945a50b8a
Ryan Feline <ryan.feline@novoda.com>
2017-03-30 17:08:47 +0100

    Add tests for hasActiveDownload.

--- library/src/test/java/com/novoda/downloadmanager/service/DownloadUpdaterTest.java
+++ library/src/test/java/com/novoda/downloadmanager/service/DownloadUpdaterTest.java
@@ -5,6 +5,7 @@ import com.novoda.downloadmanager.client.DownloadCheck;
 import com.novoda.downloadmanager.domain.Download;
 import com.novoda.downloadmanager.domain.DownloadFile;
 import com.novoda.downloadmanager.domain.DownloadId;
+import com.novoda.downloadmanager.domain.DownloadStage;
 import com.novoda.downloadmanager.download.DownloadHandler;
 import com.novoda.notils.logger.simple.Log;
 
@@ -20,6 +21,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.verify;
@@ -30,17 +32,21 @@ public class DownloadUpdaterTest {
     private static final DownloadFile UNKNOWN_SIZE_DOWNLOAD_FILE = DownloadFileFixtures.aDownloadFile().withTotalSize(-1).build();
     private static final DownloadFile ANOTHER_UNKNOWN_SIZE_DOWNLOAD_FILE = DownloadFileFixtures.aDownloadFile().withTotalSize(-1).build();
 
+    private static final Download DOWNLOAD_WITH_UNKNOWN_FILE_SIZE = DownloadFixtures.aDownload()
+            .withDownloadFiles(
+                    Arrays.asList(DOWNLOAD_FILE, UNKNOWN_SIZE_DOWNLOAD_FILE)
+            ).build();
+
+    private static final Download DOWNLOAD_WITH_ANOTHER_UNKNOWN_FILE_SIZE = DownloadFixtures.aDownload()
+            .withDownloadId(new DownloadId(67890))
+            .withDownloadFiles(Arrays.asList(
+                    DOWNLOAD_FILE, ANOTHER_UNKNOWN_SIZE_DOWNLOAD_FILE
+            ))
+            .build();
+
     private static final List<Download> DOWNLOADS = Arrays.asList(
-            DownloadFixtures.aDownload()
-                    .withDownloadFiles(
-                            Arrays.asList(DOWNLOAD_FILE, UNKNOWN_SIZE_DOWNLOAD_FILE)
-                    ).build(),
-            DownloadFixtures.aDownload()
-                    .withDownloadId(new DownloadId(67890))
-                    .withDownloadFiles(Arrays.asList(
-                            DOWNLOAD_FILE, ANOTHER_UNKNOWN_SIZE_DOWNLOAD_FILE
-                    ))
-                    .build()
+            DOWNLOAD_WITH_UNKNOWN_FILE_SIZE,
+            DOWNLOAD_WITH_ANOTHER_UNKNOWN_FILE_SIZE
     );
 
     @Rule
@@ -89,5 +95,36 @@ public class DownloadUpdaterTest {
         InOrder inOrder = inOrder(downloadHandler);
         inOrder.verify(downloadHandler).updateFileSize(UNKNOWN_SIZE_DOWNLOAD_FILE);
         inOrder.verify(downloadHandler).updateFileSize(ANOTHER_UNKNOWN_SIZE_DOWNLOAD_FILE);
+    }
+
+    @Test
+    public void givenDownloads_withSubmittedDownload_whenUpdating_thenReturnsTrue() {
+        List<Download> downloads = givenDownloadsWith(DownloadStage.SUBMITTED);
+        given(downloadHandler.getAllDownloads()).willReturn(downloads);
+
+        boolean isActivelyDownloading = downloadUpdater.update();
+
+        assertThat(isActivelyDownloading).isTrue();
+    }
+
+    @Test
+    public void givenDownloads_withRunningDownload_whenUpdating_thenReturnsTrue() {
+        List<Download> downloads = givenDownloadsWith(DownloadStage.RUNNING);
+        given(downloadHandler.getAllDownloads()).willReturn(downloads);
+
+        boolean isActivelyDownloading = downloadUpdater.update();
+
+        assertThat(isActivelyDownloading).isTrue();
+    }
+
+    private List<Download> givenDownloadsWith(DownloadStage downloadStage) {
+        return Arrays.asList(
+                DownloadFixtures.aDownload()
+                        .withDownloadStage(downloadStage)
+                        .build(),
+                DownloadFixtures.aDownload()
+                        .withDownloadId(new DownloadId(67890))
+                        .build()
+        );
     }
 }
