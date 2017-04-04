5f2a8dae04cba4acea8b62d08d1f5fee0929c5a3
Ataul Munim <hello@ataulm.com>
2017-04-04 12:35:48 +0100

    Revert position of SUBMITTED in DownloadStage

--- library/src/main/java/com/novoda/downloadmanager/domain/DownloadStage.java
+++ library/src/main/java/com/novoda/downloadmanager/domain/DownloadStage.java
@@ -1,12 +1,13 @@
 package com.novoda.downloadmanager.domain;
 
 public enum DownloadStage {
+    
     QUEUED,
+    SUBMITTED,
     RUNNING,
     PAUSED,
     COMPLETED,
     FAILED,
-    MARKED_FOR_DELETION,
-    SUBMITTED
+    MARKED_FOR_DELETION
 
 }
