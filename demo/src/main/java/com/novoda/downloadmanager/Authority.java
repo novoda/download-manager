0fd7216d1ef77a1daa57f1ad363ffef3643fb1ae
Ataul Munim <hello@ataulm.com>
2017-07-28 16:39:35 +0100

    Add note for Authority

--- demo/src/main/java/com/novoda/downloadmanager/Authority.java
+++ demo/src/main/java/com/novoda/downloadmanager/Authority.java
@@ -1,5 +1,11 @@
 package com.novoda.downloadmanager;
 
+/**
+ * This class is used by `Reflector.java` to allow the client to provide an authority.
+ *
+ * The value here must be the same as it is in AndroidManifest for the demo, injected from
+ * demo/build.gradle.
+ */
 public class Authority {
     public static final String AUTHORITY = "com.novoda.downloadmanager.demo";
 }
