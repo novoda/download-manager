1ddf03d10aa59a521611ac6b7320c580c5d604de
Ryan Feline <ryan.feline@novoda.com>
2017-11-13 11:58:54 +0000

    Use just default DM.

--- app/src/main/java/com/novoda/downloadmanager/demo/DemoApplication.java
+++ app/src/main/java/com/novoda/downloadmanager/demo/DemoApplication.java
@@ -5,14 +5,8 @@ import android.os.Handler;
 import android.os.Looper;
 
 import com.facebook.stetho.Stetho;
-import com.novoda.downloadmanager.DownloadsPersistence;
-import com.novoda.downloadmanager.FileDownloader;
-import com.novoda.downloadmanager.FileSizeRequester;
 import com.novoda.downloadmanager.DownloadManagerBuilder;
 import com.novoda.downloadmanager.LiteDownloadManagerCommands;
-import com.novoda.downloadmanager.NotificationCreator;
-
-import java.util.concurrent.TimeUnit;
 
 public class DemoApplication extends Application {
 
@@ -26,24 +20,10 @@ public class DemoApplication extends Application {
     }
 
     private void createLiteDownloadManager() {
-        FileSizeRequester fileSizeRequester = new CustomFileSizeRequester();
-        FileDownloader fileDownloader = new CustomFileDownloader();
-        DownloadsPersistence downloadsPersistence = new CustomDownloadsPersistence();
-        NotificationCreator notificationCreator = new CustomNotificationCreator(this, R.mipmap.ic_launcher_round);
-
         Handler handler = new Handler(Looper.getMainLooper());
 
         liteDownloadManagerCommands = DownloadManagerBuilder
                 .newInstance(this, handler, R.mipmap.ic_launcher_round)
-                .withFileDownloaderCustom(fileSizeRequester, fileDownloader)
-                .withFilePersistenceExternal()
-                .withFilePersistenceCustom(CustomFilePersistence.class)
-                .withDownloadsPersistenceCustom(downloadsPersistence)
-                .withNotification(notificationCreator)
-                .withNetworkRecovery(false)
-                .withCallbackThrottleCustom(CustomCallbackThrottle.class)
-                .withCallbackThrottleByTime(TimeUnit.SECONDS, 3)
-                .withCallbackThrottleByProgressIncrease()
                 .build();
     }
 
