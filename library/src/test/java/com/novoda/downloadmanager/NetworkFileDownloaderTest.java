2703344d780ed5d8010d8a6904bfe05baf114e96
Ryan Feline <ryan.feline@novoda.com>
2017-11-17 15:21:06 +0000

    Remove spy from request creator.

--- library/src/test/java/com/novoda/downloadmanager/NetworkFileDownloaderTest.java
+++ library/src/test/java/com/novoda/downloadmanager/NetworkFileDownloaderTest.java
@@ -11,50 +11,38 @@ import org.junit.Test;
 
 import static com.novoda.downloadmanager.InternalFileSizeFixtures.aFileSize;
 import static com.novoda.downloadmanager.NetworkResponseFixtures.aNetworkResponse;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 public class NetworkFileDownloaderTest {
 
     private static final byte[] BYTES_TO_RECEIVE = "s".getBytes();
+    private static final HttpClient.NetworkResponse RESPONSE_WITH_INPUT_STREAM = aNetworkResponse()
+            .withInputStream(new ByteArrayInputStream(BYTES_TO_RECEIVE))
+            .build();
 
     private static final String ANY_RAW_URL = "http://example.com";
     private static final FileSize KNOWN_FILE_SIZE = aFileSize().withAreBytesDownloadedKnown(true).build();
     private static final FileSize UNKNOWN_FILE_SIZE = aFileSize().withAreBytesDownloadedKnown(false).build();
+    private static final HttpClient.NetworkResponse INVALID_RESPONSE = aNetworkResponse().withCode(418).build();
 
     private final FileDownloader.Callback callback = mock(FileDownloader.Callback.class);
     private final HttpClient httpClient = mock(HttpClient.class);
-    private final NetworkRequestCreator requestCreator = spy(new NetworkRequestCreator());
+    private final NetworkRequestCreator requestCreator = new NetworkRequestCreator();
 
     private NetworkFileDownloader networkFileDownloader;
 
     @Before
-    public void setUp() throws IOException {
+    public void setUp() {
         Log.setShowLogs(false);
         networkFileDownloader = new NetworkFileDownloader(httpClient, requestCreator);
-        given(httpClient.execute(any(NetworkRequest.class))).willReturn(
-                aNetworkResponse().withInputStream(new ByteArrayInputStream(BYTES_TO_RECEIVE)).build()
-        );
     }
 
     @Test
-    public void createsRequestWithDownloadedBytesHeader_whenKnown() {
+    public void emitsBytes_whenDownloading() throws IOException {
+        NetworkRequest downloadRequest = requestCreator.createDownloadRequestWithDownloadedBytesHeader(ANY_RAW_URL, KNOWN_FILE_SIZE.currentSize(), KNOWN_FILE_SIZE.totalSize());
+        given(httpClient.execute(downloadRequest)).willReturn(RESPONSE_WITH_INPUT_STREAM);
         networkFileDownloader.startDownloading(ANY_RAW_URL, KNOWN_FILE_SIZE, callback);
-
-        verify(requestCreator).createDownloadRequestWithDownloadedBytesHeader(ANY_RAW_URL, KNOWN_FILE_SIZE.currentSize(), KNOWN_FILE_SIZE.totalSize());
-    }
-
-    @Test
-    public void createsDownloadRequest() {
-        networkFileDownloader.startDownloading(ANY_RAW_URL, UNKNOWN_FILE_SIZE, callback);
-
-        verify(requestCreator).createDownloadRequest(ANY_RAW_URL);
-    }
-
-    @Test
-    public void emitsBytes_whenDownloading() {
-        networkFileDownloader.startDownloading(ANY_RAW_URL, UNKNOWN_FILE_SIZE, callback);
 
         byte[] expectedBytes = new byte[4096];
         expectedBytes[0] = "s".getBytes()[0];
@@ -65,7 +53,7 @@ public class NetworkFileDownloaderTest {
     @Test
     public void closesByteStream_whenDownloading() throws IOException {
         HttpClient.NetworkResponse networkResponse = spy(aNetworkResponse().build());
-        given(httpClient.execute(any(NetworkRequest.class))).willReturn(networkResponse);
+        given(httpClient.execute(requestCreator.createDownloadRequest(ANY_RAW_URL))).willReturn(networkResponse);
 
         networkFileDownloader.startDownloading(ANY_RAW_URL, UNKNOWN_FILE_SIZE, callback);
 
@@ -74,7 +62,7 @@ public class NetworkFileDownloaderTest {
 
     @Test
     public void emitsError_whenResponseCodeIsInvalid() throws IOException {
-        given(httpClient.execute(any(NetworkRequest.class))).willReturn(aNetworkResponse().withCode(418).build());
+        given(httpClient.execute(requestCreator.createDownloadRequest(ANY_RAW_URL))).willReturn(INVALID_RESPONSE);
 
         networkFileDownloader.startDownloading(ANY_RAW_URL, UNKNOWN_FILE_SIZE, callback);
 
@@ -83,7 +71,7 @@ public class NetworkFileDownloaderTest {
 
     @Test
     public void emitsDownloadFinished_whenResponseCodeIsInvalid() throws IOException {
-        given(httpClient.execute(any(NetworkRequest.class))).willReturn(aNetworkResponse().withCode(418).build());
+        given(httpClient.execute(requestCreator.createDownloadRequest(ANY_RAW_URL))).willReturn(INVALID_RESPONSE);
 
         networkFileDownloader.startDownloading(ANY_RAW_URL, UNKNOWN_FILE_SIZE, callback);
 
@@ -92,7 +80,7 @@ public class NetworkFileDownloaderTest {
 
     @Test
     public void emitsError_whenRequestExecutionFails() throws IOException {
-        given(httpClient.execute(any(NetworkRequest.class))).willThrow(IOException.class);
+        given(httpClient.execute(requestCreator.createDownloadRequest(ANY_RAW_URL))).willThrow(IOException.class);
 
         networkFileDownloader.startDownloading(ANY_RAW_URL, UNKNOWN_FILE_SIZE, callback);
 
@@ -101,7 +89,7 @@ public class NetworkFileDownloaderTest {
 
     @Test
     public void emitsDownloadFinished_whenRequestExecutionFails() throws IOException {
-        given(httpClient.execute(any(NetworkRequest.class))).willThrow(IOException.class);
+        given(httpClient.execute(requestCreator.createDownloadRequest(ANY_RAW_URL))).willThrow(IOException.class);
 
         networkFileDownloader.startDownloading(ANY_RAW_URL, UNKNOWN_FILE_SIZE, callback);
 
