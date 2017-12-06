93ef127e02c9bc9e18347e9669fb515a92fc5bc2
Ryan Feline <ryan.feline@novoda.com>
2017-12-06 14:31:23 +0000

    Extract constants for column indices.

--- library/src/main/java/com/novoda/downloadmanager/MigrationExtractor.java
+++ library/src/main/java/com/novoda/downloadmanager/MigrationExtractor.java
@@ -12,6 +12,10 @@ class MigrationExtractor {
     private static final String BATCHES_QUERY = "SELECT batches._id, batches.batch_title FROM batches INNER JOIN DownloadsByBatch ON " +
             "DownloadsByBatch.batch_id = batches._id WHERE DownloadsByBatch.batch_total_bytes = DownloadsByBatch.batch_current_bytes";
     private static final String URIS_QUERY = "SELECT uri, _data, total_bytes FROM Downloads WHERE batch_id = ?";
+    private static final int FIRST_COLUMN = 0;
+    private static final int SECOND_COLUMN = 1;
+    private static final int THIRD_COLUMN = 2;
+
     private final SqlDatabaseWrapper database;
 
     MigrationExtractor(SqlDatabaseWrapper database) {
@@ -24,8 +28,8 @@ class MigrationExtractor {
         List<Migration> migrations = new ArrayList<>();
         while (batchesCursor.moveToNext()) {
 
-            String batchId = batchesCursor.getString(0);
-            String batchTitle = batchesCursor.getString(1);
+            String batchId = batchesCursor.getString(FIRST_COLUMN);
+            String batchTitle = batchesCursor.getString(SECOND_COLUMN);
 
             Cursor uriCursor = database.rawQuery(URIS_QUERY, batchId);
             Batch.Builder newBatchBuilder = new Batch.Builder(DownloadBatchIdCreator.createFrom(batchId), batchTitle);
@@ -34,15 +38,15 @@ class MigrationExtractor {
             List<FileSize> fileSizes = new ArrayList<>();
 
             while (uriCursor.moveToNext()) {
-                String uri = uriCursor.getString(0);
-                String originalFileName = uriCursor.getString(1);
+                String uri = uriCursor.getString(FIRST_COLUMN);
+                String originalFileName = uriCursor.getString(SECOND_COLUMN);
                 Log.d("MainActivity", batchId + " : " + batchTitle + " : " + uri);
 
                 newBatchBuilder.addFile(uri);
 
                 originalFileLocations.add(originalFileName);
 
-                long rawFileSize = uriCursor.getLong(2);
+                long rawFileSize = uriCursor.getLong(THIRD_COLUMN);
                 FileSize fileSize = new LiteFileSize(rawFileSize, rawFileSize);
                 fileSizes.add(fileSize);
             }
