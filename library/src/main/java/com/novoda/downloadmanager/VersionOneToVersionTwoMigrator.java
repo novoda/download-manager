115244d2b85fb65623a964540c6c42fb32dbd6d2
frankie-boy <frankieboylan@hotmail.co.uk>
2017-12-12 15:52:43 +0000

    migrate batch by batch

--- library/src/main/java/com/novoda/downloadmanager/VersionOneToVersionTwoMigrator.java
+++ library/src/main/java/com/novoda/downloadmanager/VersionOneToVersionTwoMigrator.java
@@ -47,14 +47,16 @@ class VersionOneToVersionTwoMigrator implements Migrator {
 
         migrationCallback.onUpdate("Migrating Files");
         Log.d(TAG, "about to migrate the files, time is " + System.nanoTime());
-        migrateV1FilesToV2Location(migrations);
-        Log.d(TAG, "files are all MOVED, about to start migrating the database, time is " + System.nanoTime());
 
-        migrateV1DataToV2Database(migrations);
+        for (Migration migration : migrations) {
+            migrateV1FilesToV2Location(migration);
+            migrateV1DataToV2Database(migration);
+            deleteFrom(database, migration);
+        }
+
         Log.d(TAG, "all data migrations are COMMITTED, about to delete the old database, time is " + System.nanoTime());
 
         migrationCallback.onUpdate("Deleting Database");
-        deleteFrom(database, migrations);
         Log.d(TAG, "all traces of v1 are ERASED, time is " + System.nanoTime());
         database.close();
 
@@ -62,88 +64,82 @@ class VersionOneToVersionTwoMigrator implements Migrator {
         migrationCallback.onUpdate("Migration Complete");
     }
 
-    private void migrateV1FilesToV2Location(List<Migration> migrations) {
-        for (Migration migration : migrations) {
-            Batch batch = migration.batch();
-            for (Migration.FileMetadata fileMetadata : migration.getFileMetadata()) {
-                FileName newFileName = LiteFileName.from(batch, fileMetadata.uri());
-                internalFilePersistence.create(newFileName, fileMetadata.fileSize());
-                FileInputStream inputStream = null;
-                try {
-                    // open the v1 file
-                    inputStream = new FileInputStream(new File(fileMetadata.originalFileLocation()));
-                    byte[] bytes = new byte[RANDOMLY_CHOSEN_BUFFER_SIZE_THAT_SEEMS_TO_WORK];
+    private void migrateV1FilesToV2Location(Migration migration) {
+        Batch batch = migration.batch();
+        for (Migration.FileMetadata fileMetadata : migration.getFileMetadata()) {
+            FileName newFileName = LiteFileName.from(batch, fileMetadata.uri());
+            internalFilePersistence.create(newFileName, fileMetadata.fileSize());
+            FileInputStream inputStream = null;
+            try {
+                // open the v1 file
+                inputStream = new FileInputStream(new File(fileMetadata.originalFileLocation()));
+                byte[] bytes = new byte[RANDOMLY_CHOSEN_BUFFER_SIZE_THAT_SEEMS_TO_WORK];
 
-                    // read the v1 file
-                    int readLast = 0;
-                    while (readLast != -1) {
-                        readLast = inputStream.read(bytes);
-                        if (readLast != 0 && readLast != -1) {
-                            // write the v1 file to the v2 location
-                            internalFilePersistence.write(bytes, 0, readLast);
-                            bytes = new byte[RANDOMLY_CHOSEN_BUFFER_SIZE_THAT_SEEMS_TO_WORK];
-                        }
+                // read the v1 file
+                int readLast = 0;
+                while (readLast != -1) {
+                    readLast = inputStream.read(bytes);
+                    if (readLast != 0 && readLast != -1) {
+                        // write the v1 file to the v2 location
+                        internalFilePersistence.write(bytes, 0, readLast);
+                        bytes = new byte[RANDOMLY_CHOSEN_BUFFER_SIZE_THAT_SEEMS_TO_WORK];
+                    }
+                }
+            } catch (IOException e) {
+                Log.e(getClass().getSimpleName(), e.getMessage());
+                e.printStackTrace();
+            } finally {
+                try {
+                    internalFilePersistence.close();
+                    if (inputStream != null) {
+                        inputStream.close();
                     }
                 } catch (IOException e) {
                     Log.e(getClass().getSimpleName(), e.getMessage());
                     e.printStackTrace();
-                } finally {
-                    try {
-                        internalFilePersistence.close();
-                        if (inputStream != null) {
-                            inputStream.close();
-                        }
-                    } catch (IOException e) {
-                        Log.e(getClass().getSimpleName(), e.getMessage());
-                        e.printStackTrace();
-                    }
                 }
             }
         }
     }
 
-    private void migrateV1DataToV2Database(List<Migration> migrations) {
-        for (Migration migration : migrations) {
-            Batch batch = migration.batch();
+    private void migrateV1DataToV2Database(Migration migration) {
+        Batch batch = migration.batch();
 
-            downloadsPersistence.startTransaction();
+        downloadsPersistence.startTransaction();
 
-            DownloadBatchTitle downloadBatchTitle = new LiteDownloadBatchTitle(batch.getTitle());
-            DownloadsBatchPersisted persistedBatch = new LiteDownloadsBatchPersisted(downloadBatchTitle, batch.getDownloadBatchId(), Status.DOWNLOADED);
-            downloadsPersistence.persistBatch(persistedBatch);
+        DownloadBatchTitle downloadBatchTitle = new LiteDownloadBatchTitle(batch.getTitle());
+        DownloadsBatchPersisted persistedBatch = new LiteDownloadsBatchPersisted(downloadBatchTitle, batch.getDownloadBatchId(), Status.DOWNLOADED);
+        downloadsPersistence.persistBatch(persistedBatch);
 
-            for (Migration.FileMetadata fileMetadata : migration.getFileMetadata()) {
-                String url = fileMetadata.uri();
+        for (Migration.FileMetadata fileMetadata : migration.getFileMetadata()) {
+            String url = fileMetadata.uri();
 
-                FileName fileName = LiteFileName.from(batch, url);
-                FilePath filePath = FilePathCreator.create(fileName.name());
-                DownloadFileId downloadFileId = DownloadFileId.from(batch);
-                DownloadsFilePersisted persistedFile = new LiteDownloadsFilePersisted(
-                        batch.getDownloadBatchId(),
-                        downloadFileId,
-                        fileName,
-                        filePath,
-                        fileMetadata.fileSize().totalSize(),
-                        url,
-                        FilePersistenceType.INTERNAL
-                );
-                downloadsPersistence.persistFile(persistedFile);
-            }
-
-            downloadsPersistence.transactionSuccess();
-            downloadsPersistence.endTransaction();
+            FileName fileName = LiteFileName.from(batch, url);
+            FilePath filePath = FilePathCreator.create(fileName.name());
+            DownloadFileId downloadFileId = DownloadFileId.from(batch);
+            DownloadsFilePersisted persistedFile = new LiteDownloadsFilePersisted(
+                    batch.getDownloadBatchId(),
+                    downloadFileId,
+                    fileName,
+                    filePath,
+                    fileMetadata.fileSize().totalSize(),
+                    url,
+                    FilePersistenceType.INTERNAL
+            );
+            downloadsPersistence.persistFile(persistedFile);
         }
+
+        downloadsPersistence.transactionSuccess();
+        downloadsPersistence.endTransaction();
     }
 
     // TODO: See https://github.com/novoda/download-manager/issues/270
-    private void deleteFrom(SqlDatabaseWrapper database, List<Migration> migrations) {
-        for (Migration migration : migrations) {
-            Batch batch = migration.batch();
-            database.rawQuery(DELETE_BY_ID_QUERY, batch.getDownloadBatchId().stringValue());
-            for (Migration.FileMetadata metadata : migration.getFileMetadata()) {
-                File file = new File(metadata.originalFileLocation());
-                file.delete();
-            }
+    private void deleteFrom(SqlDatabaseWrapper database, Migration migration) {
+        Batch batch = migration.batch();
+        database.rawQuery(DELETE_BY_ID_QUERY, batch.getDownloadBatchId().stringValue());
+        for (Migration.FileMetadata metadata : migration.getFileMetadata()) {
+            File file = new File(metadata.originalFileLocation());
+            file.delete();
         }
     }
 }
