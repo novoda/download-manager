614a7c9d8c68d514f400db640f6bf6edcb6453b6
Joe Timmins <joe@novoda.com>
2017-12-13 17:42:28 +0000

    Remove unused dao method

--- library/src/main/java/com/novoda/downloadmanager/RoomFileDao.java
+++ library/src/main/java/com/novoda/downloadmanager/RoomFileDao.java
@@ -15,7 +15,4 @@ interface RoomFileDao {
 
     @Query("SELECT * FROM RoomFile WHERE RoomFile.batch_id = :batchId")
     List<RoomFile> loadAllFilesFor(String batchId);
-
-    @Query("SELECT * FROM RoomFile")
-    List<RoomFile> loadAllFiles();
 }
