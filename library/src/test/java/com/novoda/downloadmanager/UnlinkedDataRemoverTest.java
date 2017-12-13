4fac5356a8ed579e8f9ece5c5d4c5a559244dcd4
Joe Timmins <joe@novoda.com>
2017-12-13 17:02:05 +0000

    Use fixtures in test for UnlinkedDataRemover

--- library/src/test/java/com/novoda/downloadmanager/UnlinkedDataRemoverTest.java
+++ library/src/test/java/com/novoda/downloadmanager/UnlinkedDataRemoverTest.java
@@ -7,6 +7,8 @@ import java.util.List;
 import org.junit.Test;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.novoda.downloadmanager.DownloadsBatchPersistedFixtures.aDownloadsBatchPersisted;
+import static com.novoda.downloadmanager.DownloadsFilePersistedFixtures.aDownloadsFilePersisted;
 
 public class UnlinkedDataRemoverTest {
 
@@ -63,7 +65,6 @@ public class UnlinkedDataRemoverTest {
 
     private class DummyDownloadsPersistence implements DownloadsPersistence {
 
-
         @Override
         public void startTransaction() {
 
@@ -86,8 +87,9 @@ public class UnlinkedDataRemoverTest {
 
         @Override
         public List<DownloadsBatchPersisted> loadBatches() {
-            DownloadsBatchPersisted firstBatch = buildDownloadsBatchPersisted("first batch title", "first batch id");
-            DownloadsBatchPersisted secondBatch = buildDownloadsBatchPersisted("second batch title", "second batch id");
+            DownloadsBatchPersisted firstBatch = aDownloadsBatchPersisted().withDownloadBatchId("first batch id").withDownloadBatchTitle("first batch title").build();
+            DownloadsBatchPersisted secondBatch = aDownloadsBatchPersisted().withDownloadBatchId("second batch id").withDownloadBatchTitle("second batch title").build();
+
             return Arrays.asList(firstBatch, secondBatch);
         }
 
@@ -98,53 +100,9 @@ public class UnlinkedDataRemoverTest {
 
         @Override
         public List<DownloadsFilePersisted> loadFiles(final DownloadBatchId batchId) {
-            DownloadsFilePersisted firstFile = buildDownloadsFilePersisted(batchId, LINKED_FIRST_FILENAME);
-            DownloadsFilePersisted secondFile = buildDownloadsFilePersisted(batchId, LINKED_SECOND_FILENAME);
+            DownloadsFilePersisted firstFile = aDownloadsFilePersisted().withDownloadBatchId(batchId).withFileName(LINKED_FIRST_FILENAME).build();
+            DownloadsFilePersisted secondFile = aDownloadsFilePersisted().withDownloadBatchId(batchId).withFileName(LINKED_SECOND_FILENAME).build();
             return Arrays.asList(firstFile, secondFile);
-        }
-
-        private DownloadsFilePersisted buildDownloadsFilePersisted(final DownloadBatchId batchId, final String filename) {
-            return new DownloadsFilePersisted() {
-                @Override
-                public DownloadBatchId downloadBatchId() {
-                    return batchId;
-                }
-
-                @Override
-                public FileName fileName() {
-                    return new FileName() {
-                        @Override
-                        public String name() {
-                            return filename;
-                        }
-                    };
-                }
-
-                @Override
-                public FilePath filePath() {
-                    return null;
-                }
-
-                @Override
-                public long totalFileSize() {
-                    return 0;
-                }
-
-                @Override
-                public String url() {
-                    return null;
-                }
-
-                @Override
-                public DownloadFileId downloadFileId() {
-                    return null;
-                }
-
-                @Override
-                public FilePersistenceType filePersistenceType() {
-                    return null;
-                }
-            };
         }
 
         @Override
@@ -155,43 +113,6 @@ public class UnlinkedDataRemoverTest {
         @Override
         public void update(DownloadBatchId downloadBatchId, DownloadBatchStatus.Status status) {
 
-        }
-
-        private DownloadsBatchPersisted buildDownloadsBatchPersisted(final String title, final String id) {
-            return new DownloadsBatchPersisted() {
-                @Override
-                public DownloadBatchId downloadBatchId() {
-                    return buildDownloadBatchId(id);
-                }
-
-                @Override
-                public DownloadBatchStatus.Status downloadBatchStatus() {
-                    return DownloadBatchStatus.Status.DOWNLOADED;
-                }
-
-                @Override
-                public DownloadBatchTitle downloadBatchTitle() {
-                    return buildDownloadBatchTitle(title);
-                }
-            };
-        }
-
-        private DownloadBatchId buildDownloadBatchId(final String id) {
-            return new DownloadBatchId() {
-                @Override
-                public String stringValue() {
-                    return id;
-                }
-            };
-        }
-
-        private DownloadBatchTitle buildDownloadBatchTitle(final String title) {
-            return new DownloadBatchTitle() {
-                @Override
-                public String asString() {
-                    return title;
-                }
-            };
         }
     }
 }
