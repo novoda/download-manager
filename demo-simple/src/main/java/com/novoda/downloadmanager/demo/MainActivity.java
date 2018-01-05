1deedbd150847635aa26cdc366ffecffd14c2acc
Ryan Feline <ryan.feline@novoda.com>
2018-01-05 10:16:42 +0000

    Link to GH issue.

--- demo-simple/src/main/java/com/novoda/downloadmanager/demo/MainActivity.java
+++ demo-simple/src/main/java/com/novoda/downloadmanager/demo/MainActivity.java
@@ -33,7 +33,7 @@ import java.util.List;
 
 import static com.novoda.downloadmanager.DownloadBatchStatus.Status.ERROR;
 
-// Need to extract collaborators for this demo to reduce complexity.
+// Need to extract collaborators for this demo to reduce complexity. GH Issue #286
 @SuppressWarnings({"PMD.CyclomaticComplexity", "PMD.StdCyclomaticComplexity", "PMD.ModifiedCyclomaticComplexity"})
 public class MainActivity extends AppCompatActivity {
 
