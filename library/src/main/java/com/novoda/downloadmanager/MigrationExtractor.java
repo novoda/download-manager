73bb76847eeac31e3c4ccfc185af5d448a3e808c
Ryan Feline <ryan.feline@novoda.com>
2018-01-29 10:17:10 +0000

    More safely access cursors.

--- library/src/main/java/com/novoda/downloadmanager/MigrationExtractor.java
+++ library/src/main/java/com/novoda/downloadmanager/MigrationExtractor.java
@@ -3,6 +3,7 @@ package com.novoda.downloadmanager;
 import android.database.Cursor;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class MigrationExtractor {
@@ -28,38 +29,54 @@ class MigrationExtractor {
     List<Migration> extractMigrations() {
         Cursor batchesCursor = database.rawQuery(BATCHES_QUERY);
 
-        List<Migration> migrations = new ArrayList<>();
-        while (batchesCursor.moveToNext()) {
-
-            String batchId = batchesCursor.getString(BATCH_ID_COLUMN);
-            String batchTitle = batchesCursor.getString(TITLE_COLUMN);
-            long downloadedDateTimeInMillis = batchesCursor.getLong(MODIFIED_TIMESTAMP_COLUMN);
-
-            Batch.Builder newBatchBuilder = new Batch.Builder(DownloadBatchIdCreator.createFrom(batchId), batchTitle);
-            List<Migration.FileMetadata> fileMetadataList = extractFileMetadataFrom(batchId, newBatchBuilder);
-
-            Batch batch = newBatchBuilder.build();
-            migrations.add(new Migration(batch, fileMetadataList, downloadedDateTimeInMillis));
+        if (batchesCursor == null) {
+            return Collections.emptyList();
         }
-        batchesCursor.close();
-        return migrations;
+
+        try {
+            List<Migration> migrations = new ArrayList<>();
+
+            while (batchesCursor.moveToNext()) {
+                String batchId = batchesCursor.getString(BATCH_ID_COLUMN);
+                String batchTitle = batchesCursor.getString(TITLE_COLUMN);
+                long downloadedDateTimeInMillis = batchesCursor.getLong(MODIFIED_TIMESTAMP_COLUMN);
+
+                Batch.Builder newBatchBuilder = new Batch.Builder(DownloadBatchIdCreator.createFrom(batchId), batchTitle);
+                List<Migration.FileMetadata> fileMetadataList = extractFileMetadataFrom(batchId, newBatchBuilder);
+
+                Batch batch = newBatchBuilder.build();
+                migrations.add(new Migration(batch, fileMetadataList, downloadedDateTimeInMillis));
+            }
+
+            return migrations;
+        } finally {
+            batchesCursor.close();
+        }
     }
 
     private List<Migration.FileMetadata> extractFileMetadataFrom(String batchId, Batch.Builder newBatchBuilder) {
         Cursor downloadsCursor = database.rawQuery(DOWNLOADS_QUERY, batchId);
-        List<Migration.FileMetadata> fileMetadataList = new ArrayList<>();
 
-        while (downloadsCursor.moveToNext()) {
-            String originalNetworkAddress = downloadsCursor.getString(NETWORK_ADDRESS_COLUMN);
-            String originalFileLocation = downloadsCursor.getString(FILE_LOCATION_COLUMN);
-            newBatchBuilder.addFile(originalNetworkAddress);
-
-            long rawFileSize = downloadsCursor.getLong(FILE_SIZE_COLUMN);
-            FileSize fileSize = new LiteFileSize(rawFileSize, rawFileSize);
-            Migration.FileMetadata fileMetadata = new Migration.FileMetadata(originalFileLocation, fileSize, originalNetworkAddress);
-            fileMetadataList.add(fileMetadata);
+        if (downloadsCursor == null) {
+            return Collections.emptyList();
         }
-        downloadsCursor.close();
-        return fileMetadataList;
+
+        try {
+            List<Migration.FileMetadata> fileMetadataList = new ArrayList<>();
+
+            while (downloadsCursor.moveToNext()) {
+                String originalNetworkAddress = downloadsCursor.getString(NETWORK_ADDRESS_COLUMN);
+                String originalFileLocation = downloadsCursor.getString(FILE_LOCATION_COLUMN);
+                newBatchBuilder.addFile(originalNetworkAddress);
+
+                long rawFileSize = downloadsCursor.getLong(FILE_SIZE_COLUMN);
+                FileSize fileSize = new LiteFileSize(rawFileSize, rawFileSize);
+                Migration.FileMetadata fileMetadata = new Migration.FileMetadata(originalFileLocation, fileSize, originalNetworkAddress);
+                fileMetadataList.add(fileMetadata);
+            }
+            return fileMetadataList;
+        } finally {
+            downloadsCursor.close();
+        }
     }
 }
