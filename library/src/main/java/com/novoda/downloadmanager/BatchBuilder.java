f4aea85d4daecba6ab6dba13fb12953b3b938b91
Ryan Feline <ryan.feline@novoda.com>
2018-04-26 21:17:37 +0100

    Add javadoc for BatchBuilder.

--- library/src/main/java/com/novoda/downloadmanager/BatchBuilder.java
+++ library/src/main/java/com/novoda/downloadmanager/BatchBuilder.java
@@ -1,7 +1,23 @@
 package com.novoda.downloadmanager;
 
+/**
+ * Builds instances of {@link Batch} using a fluent API.
+ */
 public interface BatchBuilder {
+
+    /**
+     * Sets {@link BatchFileBuilder} to build a {@link Batch} that will download a {@link BatchFile}
+     * from a given {@param networkAddress}.
+     *
+     * @param networkAddress to download file from.
+     * @return {@link BatchFileBuilder}.
+     */
     BatchFileBuilder downloadFrom(String networkAddress);
 
+    /**
+     * Build a new {@link Batch} instance.
+     *
+     * @return an instance of {@link Batch}.
+     */
     Batch build();
 }
