d7b9ce7c1cfa4ef6b70c8f9751bb2652f15dd665
Ryan Feline <ryan.feline@novoda.com>
2018-06-20 15:51:44 +0100

    Add javadoc to the FileSizeRequester.

--- library/src/main/java/com/novoda/downloadmanager/FileSizeRequester.java
+++ library/src/main/java/com/novoda/downloadmanager/FileSizeRequester.java
@@ -1,5 +1,8 @@
 package com.novoda.downloadmanager;
 
+/**
+ * For defining the mechanism by which a file size is determined.
+ */
 public interface FileSizeRequester {
 
     FileSize requestFileSize(String url);
