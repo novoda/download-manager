06fbc4a4e97e847485fa0f6ae3dc64ce63bc3c3f
Ryan Feline <ryan.feline@novoda.com>
2018-06-20 15:36:32 +0100

    Add javadoc to the DownloadsFilePersisted.

--- library/src/main/java/com/novoda/downloadmanager/DownloadsFilePersisted.java
+++ library/src/main/java/com/novoda/downloadmanager/DownloadsFilePersisted.java
@@ -1,5 +1,8 @@
 package com.novoda.downloadmanager;
 
+/**
+ * Defines the information that is stored in the persistence layer for a {@link BatchFile}.
+ */
 public interface DownloadsFilePersisted {
 
     DownloadBatchId downloadBatchId();
