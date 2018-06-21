35d094401b4d3e997411a6285ea03366a46a25ca
Ryan Feline <ryan.feline@novoda.com>
2018-06-21 12:12:49 +0100

    Rename confusing field.

--- library/src/main/java/com/novoda/downloadmanager/StorageRequirementRules.java
+++ library/src/main/java/com/novoda/downloadmanager/StorageRequirementRules.java
@@ -6,23 +6,23 @@ import java.util.List;
 
 final class StorageRequirementRules implements StorageRequirementRule {
 
-    private List<StorageRequirementRule> storageRequirementRules;
+    private List<StorageRequirementRule> rules;
 
     static StorageRequirementRules newInstance() {
         return new StorageRequirementRules(new ArrayList<>());
     }
 
-    private StorageRequirementRules(List<StorageRequirementRule> storageRequirementRules) {
-        this.storageRequirementRules = storageRequirementRules;
+    private StorageRequirementRules(List<StorageRequirementRule> rules) {
+        this.rules = rules;
     }
 
     void addRule(StorageRequirementRule storageRequirementRule) {
-        storageRequirementRules.add(storageRequirementRule);
+        rules.add(storageRequirementRule);
     }
 
     @Override
     public boolean hasViolatedRule(File storageDirectory, FileSize downloadFileSize) {
-        for (StorageRequirementRule requirementRule : storageRequirementRules) {
+        for (StorageRequirementRule requirementRule : rules) {
             if (requirementRule.hasViolatedRule(storageDirectory, downloadFileSize)) {
                 return true;
             }
