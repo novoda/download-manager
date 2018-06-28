60abd8660cda4e9b6567751463b9e99c7aa443d1
Ryan Feline <ryan.feline@novoda.com>
2018-06-28 11:28:45 +0100

    Extract storage root to constant.

--- library/src/test/java/com/novoda/downloadmanager/MigrationExtractorTest.java
+++ library/src/test/java/com/novoda/downloadmanager/MigrationExtractorTest.java
@@ -47,6 +47,8 @@ public class MigrationExtractorTest {
             .with("notificationextras", "file_3", "file_4")
             .build();
 
+    private static final StorageRoot STORAGE_ROOT = TestStorageRootFactory.create();
+
     private final SqlDatabaseWrapper database = mock(SqlDatabaseWrapper.class);
     private final FilePersistence filePersistence = mock(FilePersistence.class);
 
@@ -54,7 +56,7 @@ public class MigrationExtractorTest {
 
     @Before
     public void setUp() {
-        migrationExtractor = new MigrationExtractor(TestStorageRootFactory.create(), database, filePersistence, "base");
+        migrationExtractor = new MigrationExtractor(STORAGE_ROOT, database, filePersistence, "base");
     }
 
     @Test
@@ -76,7 +78,7 @@ public class MigrationExtractorTest {
     private List<Migration> expectedMigrations() {
         String firstUri = "uri_1";
         String secondUri = "uri_2";
-        Batch firstBatch = Batch.with(TestStorageRootFactory.create(), DownloadBatchIdCreator.createSanitizedFrom(String.valueOf("file_1".hashCode())), "title_1")
+        Batch firstBatch = Batch.with(STORAGE_ROOT, DownloadBatchIdCreator.createSanitizedFrom(String.valueOf("file_1".hashCode())), "title_1")
                 .downloadFrom(firstUri).withIdentifier(DownloadFileIdCreator.createFrom("file_1")).apply()
                 .downloadFrom(secondUri).withIdentifier(DownloadFileIdCreator.createFrom("file_2")).apply()
                 .build();
@@ -87,7 +89,7 @@ public class MigrationExtractorTest {
 
         String thirdUri = "uri_3";
         String fourthUri = "uri_4";
-        Batch secondBatch = Batch.with(TestStorageRootFactory.create(), DownloadBatchIdCreator.createSanitizedFrom(String.valueOf("file_3".hashCode())), "title_2")
+        Batch secondBatch = Batch.with(STORAGE_ROOT, DownloadBatchIdCreator.createSanitizedFrom(String.valueOf("file_3".hashCode())), "title_2")
                 .downloadFrom(thirdUri).withIdentifier(DownloadFileIdCreator.createFrom("file_3")).apply()
                 .downloadFrom(fourthUri).withIdentifier(DownloadFileIdCreator.createFrom("file_4")).apply()
                 .build();
