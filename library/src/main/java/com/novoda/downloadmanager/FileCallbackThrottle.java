9dac9db8ed3e2bb57cdcafcedccb1cc380a8957a
Ryan Feline <ryan.feline@novoda.com>
2018-07-04 09:37:31 +0100

    Add javadoc to the CallbackThrottle.

--- library/src/main/java/com/novoda/downloadmanager/FileCallbackThrottle.java
+++ library/src/main/java/com/novoda/downloadmanager/FileCallbackThrottle.java
@@ -1,10 +1,30 @@
 package com.novoda.downloadmanager;
 
+/**
+ * Controls the rate at which a {@link DownloadFile} calls back to
+ * a {@link DownloadBatch}. Essentially controls the rate of notifications
+ * during a download of a file.
+ */
 public interface FileCallbackThrottle {
 
+    /**
+     * Set the callback that should be notified of {@link DownloadBatchStatus}
+     * changes.
+     *
+     * @param callback to notify.
+     */
     void setCallback(DownloadBatchStatusCallback callback);
 
+    /**
+     * The throttle logic that will forward events to the {@link DownloadBatchStatusCallback}.
+     *
+     * @param downloadBatchStatus the latest status to emit.
+     */
     void update(DownloadBatchStatus downloadBatchStatus);
 
+    /**
+     * Disable the callback or the throttling logic to
+     * prevent further emissions of {@link DownloadBatchStatus}.
+     */
     void stopUpdates();
 }
