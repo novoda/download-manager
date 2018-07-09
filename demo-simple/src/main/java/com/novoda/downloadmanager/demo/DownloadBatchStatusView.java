76621768251ad1299340b09e5ae7cb3a4265ff5a
Ryan Feline <ryan.feline@novoda.com>
2018-07-09 16:36:08 +0100

    Create a merge for a download batch status.

---
+++ demo-simple/src/main/java/com/novoda/downloadmanager/demo/DownloadBatchStatusView.java
@@ -0,0 +1,13 @@
+package com.novoda.downloadmanager.demo;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+
+public class DownloadBatchStatusView extends ConstraintLayout {
+
+    public DownloadBatchStatusView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
