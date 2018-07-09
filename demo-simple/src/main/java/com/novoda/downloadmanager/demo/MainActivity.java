212b2fbbf2357e718855fce765e432af90d83bfd
Ryan Feline <ryan.feline@novoda.com>
2018-07-09 15:09:24 +0100

    Remove migration code from the main activity.

--- demo-simple/src/main/java/com/novoda/downloadmanager/demo/MainActivity.java
+++ demo-simple/src/main/java/com/novoda/downloadmanager/demo/MainActivity.java
@@ -1,15 +1,11 @@
 package com.novoda.downloadmanager.demo;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.novoda.downloadmanager.AllBatchStatusesCallback;
@@ -21,11 +17,7 @@ import com.novoda.downloadmanager.DownloadBatchStatus;
 import com.novoda.downloadmanager.DownloadBatchStatusCallback;
 import com.novoda.downloadmanager.DownloadFileId;
 import com.novoda.downloadmanager.DownloadFileIdCreator;
-import com.novoda.downloadmanager.DownloadMigrator;
-import com.novoda.downloadmanager.DownloadMigratorBuilder;
 import com.novoda.downloadmanager.LiteDownloadManagerCommands;
-import com.novoda.downloadmanager.MigrationCallback;
-import com.novoda.downloadmanager.MigrationStatus;
 import com.novoda.downloadmanager.StorageRoot;
 import com.novoda.downloadmanager.StorageRootFactory;
 
@@ -39,9 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    @SuppressLint("SdCardPath")
-    private static final String V1_BASE_PATH = "/data/data/com.novoda.downloadmanager.demo.simple/files/Pictures/";
-
     private static final DownloadBatchId BATCH_ID_1 = DownloadBatchIdCreator.createSanitizedFrom("batch_id_1");
     private static final DownloadBatchId BATCH_ID_2 = DownloadBatchIdCreator.createSanitizedFrom("batch_id_2");
     private static final DownloadFileId FILE_ID_1 = DownloadFileIdCreator.createFrom("file_id_1");
@@ -49,14 +38,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String TEN_MB_FILE_URL = "http://ipv4.download.thinkbroadband.com/10MB.zip";
     private static final String TWENTY_MB_FILE_URL = "http://ipv4.download.thinkbroadband.com/20MB.zip";
 
-    private TextView databaseCloningUpdates;
-    private TextView databaseMigrationUpdates;
     private TextView textViewBatch1;
     private TextView textViewBatch2;
     private LiteDownloadManagerCommands liteDownloadManagerCommands;
-    private DownloadMigrator downloadMigrator;
-    private VersionOneDatabaseCloner versionOneDatabaseCloner;
-    private Spinner downloadFileSizeSpinner;
     private StorageRoot primaryStorageWithDownloadsSubpackage;
 
     @Override
@@ -64,33 +48,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        downloadMigrator = DownloadMigratorBuilder.newInstance(this, R.mipmap.ic_launcher_round)
-                .withNotificationChannel(
-                        "chocolate",
-                        "Migration notifications",
-                        NotificationManagerCompat.IMPORTANCE_DEFAULT
-                )
-                .withMigrationCallback(migrationCallback)
-                .build();
-
         textViewBatch1 = findViewById(R.id.batch_1);
         textViewBatch2 = findViewById(R.id.batch_2);
 
-        versionOneDatabaseCloner = DatabaseClonerFactory.databaseCloner(this, cloneCallback);
-
-        downloadFileSizeSpinner = findViewById(R.id.database_download_file_size);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.file_sizes, android.R.layout.simple_spinner_item);
-        downloadFileSizeSpinner.setAdapter(adapter);
-
         primaryStorageWithDownloadsSubpackage = StorageRootFactory.createPrimaryStorageDownloadsDirectoryRoot(getApplicationContext());
-
-        databaseCloningUpdates = findViewById(R.id.database_cloning_updates);
-        View buttonCreateDB = findViewById(R.id.button_create_v1_db);
-        buttonCreateDB.setOnClickListener(createDatabaseOnClick);
-
-        databaseMigrationUpdates = findViewById(R.id.database_migration_updates);
-        View buttonMigrate = findViewById(R.id.button_migrate);
-        buttonMigrate.setOnClickListener(startMigrationOnClick);
 
         CheckBox checkWifiOnly = findViewById(R.id.check_wifi_only);
         checkWifiOnly.setOnCheckedChangeListener(wifiOnlyOnCheckedChange);
@@ -111,22 +72,6 @@ public class MainActivity extends AppCompatActivity {
 
         bindBatchViews();
     }
-
-    private final VersionOneDatabaseCloner.CloneCallback cloneCallback = updateMessage -> databaseCloningUpdates.setText(updateMessage);
-
-    private final View.OnClickListener createDatabaseOnClick = v -> {
-        String selectedFileSize = (String) downloadFileSizeSpinner.getSelectedItem();
-        versionOneDatabaseCloner.cloneDatabaseWithDownloadSize(selectedFileSize);
-    };
-
-    private final MigrationCallback migrationCallback = migrationStatus -> {
-        if (migrationStatus.status() == MigrationStatus.Status.COMPLETE) {
-            liteDownloadManagerCommands.submitAllStoredDownloads(() -> Log.d(TAG, "Migration completed, submitting all downloads"));
-        }
-        databaseMigrationUpdates.setText(migrationStatus.status().toRawValue());
-    };
-
-    private final View.OnClickListener startMigrationOnClick = v -> downloadMigrator.startMigration("migrationJob", getDatabasePath("downloads.db"), V1_BASE_PATH);
 
     private final CompoundButton.OnCheckedChangeListener wifiOnlyOnCheckedChange = (buttonView, isChecked) -> {
         LiteDownloadManagerCommands downloadManagerCommands = ((DemoApplication) getApplication()).getLiteDownloadManagerCommands();
