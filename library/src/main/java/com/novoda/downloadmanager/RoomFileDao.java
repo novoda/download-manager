11674cd53ef0329f4786f4626855d6d30cf3a885
Ryan Feline <ryan.feline@novoda.com>
2018-07-25 12:15:23 +0100

    Add transaction annotation to Room queries.

--- library/src/main/java/com/novoda/downloadmanager/RoomFileDao.java
+++ library/src/main/java/com/novoda/downloadmanager/RoomFileDao.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Transaction;
 
 import java.util.List;
 
@@ -13,9 +14,11 @@ interface RoomFileDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(RoomFile roomFile);
 
+    @Transaction
     @Query("SELECT * FROM RoomFile WHERE RoomFile.batch_id = :batchId")
     List<RoomFile> loadAllFilesFor(String batchId);
 
+    @Transaction
     @Query("SELECT * FROM RoomFile")
     List<RoomFile> loadAllFiles();
 }
