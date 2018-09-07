7bfea1a32ba490e98a712241c1417acd4db2d45e
Ryan Feline <ryan.feline@novoda.com>
2018-09-07 08:28:27 +0100

    Extract navigate method and inline.

--- demo-simple/src/main/java/com/novoda/downloadmanager/demo/LandingActivity.java
+++ demo-simple/src/main/java/com/novoda/downloadmanager/demo/LandingActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -12,18 +11,12 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        findViewById(R.id.migration).setOnClickListener(view -> navigateTo(MigrationActivity.class));
+        findViewById(R.id.downloads).setOnClickListener(view -> navigateTo(MainActivity.class));
+    }
 
-        View migrationView = findViewById(R.id.migration);
-        View downloadsView = findViewById(R.id.downloads);
-
-        migrationView.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), MigrationActivity.class);
-            startActivity(intent);
-        });
-
-        downloadsView.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-        });
+    private void navigateTo(Class<?> activityClass) {
+        Intent intent = new Intent(getApplicationContext(), activityClass);
+        startActivity(intent);
     }
 }
