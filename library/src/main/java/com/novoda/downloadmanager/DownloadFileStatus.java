74bbe57f6724cdab2bb162a635a4537de3db6908
Ryan Feline <ryan.feline@novoda.com>
2018-09-11 15:29:25 +0100

    Add javadoc to the DownloadFileStatus.

--- library/src/main/java/com/novoda/downloadmanager/DownloadFileStatus.java
+++ library/src/main/java/com/novoda/downloadmanager/DownloadFileStatus.java
@@ -1,7 +1,13 @@
 package com.novoda.downloadmanager;
 
+/**
+ * Represents the information of a {@link DownloadFile} that is accessible to clients.
+ */
 public interface DownloadFileStatus {
 
+    /**
+     * The current download status for a file.
+     */
     enum Status {
         PAUSED,
         QUEUED,
@@ -12,15 +18,33 @@ public interface DownloadFileStatus {
         WAITING_FOR_NETWORK
     }
 
+    /**
+     * @return The unique identifier for the batch associated with this file.
+     */
     DownloadBatchId downloadBatchId();
 
+    /**
+     * @return The unique identifier for this file.
+     */
     DownloadFileId downloadFileId();
 
+    /**
+     * @return The number of bytes that have been downloaded so far.
+     */
     long bytesDownloaded();
 
+    /**
+     * @return The total number of bytes to download.
+     */
     long totalBytes();
 
+    /**
+     * @return The local path to this download file.
+     */
     FilePath localFilePath();
 
+    /**
+     * @return The current {@link DownloadFileStatus.Status} for the file.
+     */
     Status status();
 }
