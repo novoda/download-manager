6e430d41eb8ca7a9afda2e973718cfadf6d20efd
Ryan Feline <ryan.feline@novoda.com>
2018-09-13 10:17:57 +0100

    Wrap javadoc to avoid violation.

--- library/src/main/java/com/novoda/downloadmanager/DownloadFileStatusCallback.java
+++ library/src/main/java/com/novoda/downloadmanager/DownloadFileStatusCallback.java
@@ -3,8 +3,9 @@ package com.novoda.downloadmanager;
 import android.support.annotation.Nullable;
 
 /**
- * Given to the asynchronous call {@link DownloadManager#getDownloadFileStatusWithMatching(DownloadBatchId, DownloadFileId, DownloadFileStatusCallback)}
- * to receive the {@link DownloadFileStatus} of a {@link DownloadFile} matching the given {@link DownloadBatchId} and {@link DownloadFileId}.
+ * Given to the asynchronous call
+ * {@link DownloadManager#getDownloadFileStatusWithMatching(DownloadBatchId, DownloadFileId, DownloadFileStatusCallback)} to receive the
+ * {@link DownloadFileStatus} of a {@link DownloadFile} matching the given {@link DownloadBatchId} and {@link DownloadFileId}.
  */
 public interface DownloadFileStatusCallback {
 
