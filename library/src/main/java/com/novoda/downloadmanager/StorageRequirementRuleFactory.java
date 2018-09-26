5b5037ee549ad5dd5a81dc85ec6bab2730da5010
Ryan Feline <ryan.feline@novoda.com>
2018-09-26 14:48:38 +0100

    Memory should be storage.

--- library/src/main/java/com/novoda/downloadmanager/StorageRequirementRuleFactory.java
+++ library/src/main/java/com/novoda/downloadmanager/StorageRequirementRuleFactory.java
@@ -11,7 +11,7 @@ public final class StorageRequirementRuleFactory {
     /**
      * Creates a storage requirement rule, where the storage bytes must be greater than the
      * given bytes after a download completes. E.g. if specifying 100MB then the rule will
-     * be violated if the system memory remaining after the download is 99MB, preventing the
+     * be violated if the system storage remaining after the download is 99MB, preventing the
      * download from being started.
      *
      * @param bytesRemainingAfterDownload the amount of storage in bytes that must be remaining after a download completes.
@@ -24,7 +24,7 @@ public final class StorageRequirementRuleFactory {
     /**
      * Creates a storage requirement rule, where the storage bytes must be greater than the
      * a given percentage of the storage bytes after a download completes. E.g. if you specify 10% then the rule
-     * will be violated if the remaining system memory is at 9% of the total system memory, preventing the download
+     * will be violated if the remaining system storage is at 9% of the total system storage, preventing the download
      * from being started.
      *
      * @param percentageOfStorageRemaining the amount of storage, as a percentage of the system storage, that must
