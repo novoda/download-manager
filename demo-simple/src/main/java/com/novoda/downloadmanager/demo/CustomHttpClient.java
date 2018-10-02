4b0b00038c61c56b022eca9e251380a1b688938f
Ryan Feline <ryan.feline@novoda.com>
2018-10-02 13:35:31 +0100

    Add a custom http client and custom http response.

--- demo-simple/src/main/java/com/novoda/downloadmanager/demo/CustomHttpClient.java
+++ demo-simple/src/main/java/com/novoda/downloadmanager/demo/CustomHttpClient.java
@@ -4,11 +4,36 @@ import com.novoda.downloadmanager.HttpClient;
 import com.novoda.downloadmanager.NetworkRequest;
 
 import java.io.IOException;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class CustomHttpClient implements HttpClient {
 
-    @Override
-    public NetworkResponse execute(NetworkRequest networkRequest) throws IOException {
-        return null;
+    private final OkHttpClient httpClient;
+
+    CustomHttpClient(OkHttpClient httpClient) {
+        this.httpClient = httpClient;
     }
+
+    @Override
+    public NetworkResponse execute(NetworkRequest request) throws IOException {
+        Request.Builder requestBuilder = new Request.Builder()
+                .url(request.url());
+
+        if (request.method() == NetworkRequest.Method.HEAD) {
+            requestBuilder = requestBuilder.head();
+        }
+
+        for (Map.Entry<String, String> entry : request.headers().entrySet()) {
+            requestBuilder.addHeader(entry.getKey(), entry.getValue());
+        }
+
+        Call call = httpClient.newCall(requestBuilder.build());
+
+        return new CustomHttpResponse(call.execute());
+    }
+
 }
---
+++ demo-simple/src/main/java/com/novoda/downloadmanager/demo/CustomHttpResponse.java
@@ -0,0 +1,47 @@
+package com.novoda.downloadmanager.demo;
+
+import com.novoda.downloadmanager.HttpClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import okhttp3.Response;
+
+class CustomHttpResponse implements HttpClient.NetworkResponse {
+
+    private final Response response;
+
+    CustomHttpResponse(Response response) {
+        this.response = response;
+    }
+
+    @Override
+    public int code() {
+        return response.code();
+    }
+
+    @Override
+    public boolean isSuccessful() {
+        return response.isSuccessful();
+    }
+
+    @Override
+    public String header(String name, String defaultValue) {
+        return response.header(name, defaultValue);
+    }
+
+    @Override
+    public InputStream openByteStream() throws IOException {
+        return response.body().byteStream();
+    }
+
+    @Override
+    public void closeByteStream() throws IOException {
+        response.body().close();
+    }
+
+    @Override
+    public long bodyContentLength() {
+        return response.body().contentLength();
+    }
+}
