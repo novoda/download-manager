d7b956671a091647a80871752a3a2d6255a41d36
Ryan Feline <ryan.feline@novoda.com>
2018-10-23 12:10:10 +0100

    Check for non matching bytes for partial downloads.

--- demo-simple/src/main/java/com/novoda/downloadmanager/demo/migration/CompletedDownloadBatchesExtractor.java
+++ demo-simple/src/main/java/com/novoda/downloadmanager/demo/migration/CompletedDownloadBatchesExtractor.java
@@ -27,7 +27,8 @@ class CompletedDownloadBatchesExtractor {
             + "batches INNER JOIN DownloadsByBatch ON DownloadsByBatch.batch_id = batches._id "
             + "WHERE batches._id NOT IN (SELECT DownloadsByBatch.batch_id FROM DownloadsByBatch "
             + "INNER JOIN batches ON batches._id = DownloadsByBatch.batch_id "
-            + "WHERE DownloadsByBatch._data IS NULL "
+            + "WHERE DownloadsByBatch.batch_total_bytes != DownloadsByBatch.batch_current_bytes "
+            + "OR DownloadsByBatch._data IS NULL "
             + "GROUP BY DownloadsByBatch.batch_id) "
             + "GROUP BY batches._id";
 
