51d6fd0c3f5f047ab0cfb87594a48df3ed9d2a01
Uladzislau Yarmolin <lingviston1@gmail.com>
2018-10-29 15:10:12 +0300

    Ensure correct batch reference is used when downloading.

---
+++ library/src/test/java/com/novoda/downloadmanager/LiteDownloadManagerDownloaderTest.java
@@ -0,0 +1,119 @@
+package com.novoda.downloadmanager;
+
+import android.os.Handler;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.willAnswer;
+import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class LiteDownloadManagerDownloaderTest {
+
+    private final Object waitForDownloadService = new Object();
+    private final Object waitForDownloadBatchStatusCallback = new Object();
+    private final ExecutorService executor = mock(ExecutorService.class);
+    private final Handler callbackHandler = mock(Handler.class);
+    private final FileOperations fileOperations = mock(FileOperations.class);
+    private final DownloadsBatchPersistence downloadsBatchPersistence = mock(DownloadsBatchPersistence.class);
+    private final DownloadsFilePersistence downloadsFilePersistence = mock(DownloadsFilePersistence.class);
+    private final DownloadBatchStatusNotificationDispatcher notificationDispatcher = mock(DownloadBatchStatusNotificationDispatcher.class);
+    private final ConnectionChecker connectionChecker = mock(ConnectionChecker.class);
+    private final Set<DownloadBatchStatusCallback> callbacks = new HashSet<>();
+    private final CallbackThrottleCreator callbackThrottleCreator = mock(CallbackThrottleCreator.class);
+    private final DownloadBatchStatusFilter downloadBatchStatusFilter = mock(DownloadBatchStatusFilter.class);
+    private final Wait.Criteria serviceCriteria = mock(Wait.Criteria.class);
+    private final DownloadService downloadService = mock(DownloadService.class);
+
+    private final DownloadBatch downloadBatch = mock(DownloadBatch.class, Mockito.RETURNS_DEEP_STUBS);
+    private final DownloadBatch anotherDownloadBatchWithTheSameId = mock(DownloadBatch.class, Mockito.RETURNS_DEEP_STUBS);
+    private final DownloadBatchId downloadBatchId = mock(DownloadBatchId.class);
+
+    private final Map<DownloadBatchId, DownloadBatch> downloadingBatches = new HashMap<>();
+
+    private LiteDownloadManagerDownloader downloader;
+
+    @Before
+    public void setUp() {
+        setUpExecutorService();
+        setUpBatches();
+
+        downloader = new LiteDownloadManagerDownloader(
+                waitForDownloadService,
+                waitForDownloadBatchStatusCallback,
+                executor,
+                callbackHandler,
+                fileOperations,
+                downloadsBatchPersistence,
+                downloadsFilePersistence,
+                notificationDispatcher,
+                connectionChecker,
+                callbacks,
+                callbackThrottleCreator,
+                downloadBatchStatusFilter,
+                serviceCriteria
+        );
+
+        downloader.setDownloadService(downloadService);
+    }
+
+    private void setUpExecutorService() {
+        willAnswer(invocation -> {
+            ((Runnable) invocation.getArgument(0)).run();
+            return null;
+        }).given(executor).submit(any(Runnable.class));
+    }
+
+    private void setUpBatches() {
+        willReturn(downloadBatchId).given(downloadBatch).getId();
+        willReturn(downloadBatchId).given(anotherDownloadBatchWithTheSameId).getId();
+    }
+
+    @Test
+    public void addDownloadBatchIntoQueue_whenDownloadingBatchWithNewId() {
+        downloadingBatches.clear();
+
+        downloader.download(downloadBatch, downloadingBatches);
+
+        assertThat(downloadingBatches).containsEntry(downloadBatchId, downloadBatch);
+    }
+
+    @Test
+    public void downloadBatchByNewRef_whenDownloadingBatchWithNewId() {
+        downloadingBatches.clear();
+
+        downloader.download(downloadBatch, downloadingBatches);
+
+        verify(downloadService).download(eq(downloadBatch), any());
+    }
+
+    @Test
+    public void doesNotAddDownloadBatchIntoQueue_whenDownloadingBatchWithExistingId() {
+        downloadingBatches.put(downloadBatchId, anotherDownloadBatchWithTheSameId);
+
+        downloader.download(downloadBatch, downloadingBatches);
+
+        assertThat(downloadingBatches).doesNotContainEntry(downloadBatchId, downloadBatch);
+    }
+
+    @Test
+    public void downloadBatchByOldRef_whenDownloadingBatchWithExistingId() {
+        downloadingBatches.put(downloadBatchId, anotherDownloadBatchWithTheSameId);
+
+        downloader.download(downloadBatch, downloadingBatches);
+
+        verify(downloadService).download(eq(anotherDownloadBatchWithTheSameId), any());
+    }
+}
