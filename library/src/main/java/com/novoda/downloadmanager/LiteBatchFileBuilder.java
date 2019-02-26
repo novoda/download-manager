2e07e7d174fe1125263f310fc167774d4b69f529
Ryan Feline <ryan.feline@novoda.com>
2019-02-26 15:51:51 +0000

    2.3.0 dev to release (#481)
    * Add a single dismissible notification.
    
    * Add mechanism to create a `BatchBuilder` from a given batch.
    
    This will allow clients to add additional files to a batch outside of a singular flow before pushing to the `download-manager`.
    
    * Add javadoc to batch builder.
    
    * Normalise paths to avoid duplicated separators
    
    * Update dependencies
    
    * Make liteBatchFileBuilder final
    
    * Use BDD mockito
    
    * Rename elements to paths
    
    * Pull storagePath out
    
    * Invert method and rename it
    
    * Use CharSequence instead of StringBuilder
    
    * Add more separators in tests
    
    * Use split and join to improve effectiveness
    
    * Use File.separator
    
    * Fix pmd/findbugs issues
    
    * Extract regex
    
    * Up version for release.

--- library/src/main/java/com/novoda/downloadmanager/LiteBatchFileBuilder.java
+++ library/src/main/java/com/novoda/downloadmanager/LiteBatchFileBuilder.java
@@ -1,8 +1,12 @@
 package com.novoda.downloadmanager;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 final class LiteBatchFileBuilder implements InternalBatchFileBuilder {
+
+    private static final String SEPARATOR_AS_REGEX = File.separatorChar == '\\' ? "\\\\" : File.separator;
 
     private final StorageRoot storageRoot;
     private final DownloadBatchId downloadBatchId;
@@ -40,20 +44,62 @@ final class LiteBatchFileBuilder implements InternalBatchFileBuilder {
 
     @Override
     public BatchFileBuilder saveTo(String path, String fileName) {
-        this.path = Optional.fromNullable(path).filterNot("/"::equals);
+        this.path = Optional.fromNullable(path);
         this.fileName = Optional.fromNullable(fileName);
         return this;
     }
 
     @Override
     public BatchBuilder apply() {
-        String absolutePath = storageRoot.path() + File.separator
-                + downloadBatchId.rawId() + File.separator
-                + path.map((it) -> it + File.separator).or("")
-                + fileName.getOrElse(() -> FileNameExtractor.extractFrom(networkAddress));
+        String absolutePath = buildPath(
+                storageRoot.path(),
+                downloadBatchId.rawId(),
+                path.or(""),
+                fileName.getOrElse(() -> FileNameExtractor.extractFrom(networkAddress))
+        );
 
         parentBuilder.withFile(new BatchFile(networkAddress, downloadFileId, absolutePath));
         return parentBuilder;
     }
 
+    private String buildPath(String... paths) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String storagePath = paths[0];
+        if (storagePath.startsWith(File.separator)) {
+            stringBuilder.append(File.separator);
+        }
+
+        String joinedPaths = sanitise(joinWithFileSeparator(paths));
+        stringBuilder.append(joinedPaths);
+
+        return stringBuilder.toString();
+    }
+
+    private String sanitise(String path) {
+        String[] pathSegments = path.split(SEPARATOR_AS_REGEX);
+        CharSequence[] filteredPathSegments = filterEmptySegmentsOut(pathSegments);
+        return joinWithFileSeparator(filteredPathSegments);
+    }
+
+    private CharSequence[] filterEmptySegmentsOut(String... pathSegments) {
+        List<CharSequence> filteredPathSegments = new ArrayList<>();
+        for (String pathSegment : pathSegments) {
+            if (!pathSegment.isEmpty()) {
+                filteredPathSegments.add(pathSegment);
+            }
+        }
+        return filteredPathSegments.toArray(new CharSequence[filteredPathSegments.size()]);
+    }
+
+    private String joinWithFileSeparator(CharSequence... elements) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < elements.length; i++) {
+            stringBuilder.append(elements[i]);
+            if (i < elements.length - 1) {
+                stringBuilder.append((CharSequence) File.separator);
+            }
+        }
+        return stringBuilder.toString();
+    }
 }
