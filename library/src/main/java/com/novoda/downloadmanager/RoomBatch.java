d578031b05a21d7acd5f3c06a1fa4f0836efac24
Adam Brown <adampsbrown@gmail.com>
2019-09-27 11:55:40 +0100

    upgrading room to androidx and the latest version

--- library/src/main/java/com/novoda/downloadmanager/RoomBatch.java
+++ library/src/main/java/com/novoda/downloadmanager/RoomBatch.java
@@ -1,10 +1,10 @@
 package com.novoda.downloadmanager;
 
-import android.arch.persistence.room.ColumnInfo;
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.Index;
-import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
 
 @SuppressWarnings("checkstyle:visibilitymodifier") // Accessors will add a lot of boilerplate code.
 @Entity(indices = {@Index("batch_id")})
